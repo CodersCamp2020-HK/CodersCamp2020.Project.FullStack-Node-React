f12c27019a0aa69bd80a91d6255baa20d557577c
Mateusz Król <matek8952@gmail.com>
2021-02-21 13:44:04 +0100

    error before npm i

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,34 @@ import express from 'express';
 import { api } from './presentation/rest';
 import './IocContainerSetup';
 import path from 'path';
+// import { Sequelize } from 'sequelize';
+// import 'dotenv/config';
+
+// async () => {
+// try {
+//     const sequelize = new Sequelize(
+//         process.env.POSTGRES_DB as string,
+//         process.env.POSTGRES_USER as string,
+//         process.env.POSTGRES_PASSWORD,
+//         {
+//             host: process.env.POSTGRES_HOST,
+//             dialect: 'postgres',
+//         },
+//     );
+//     await sequelize.authenticate();
+//     console.log('Connected to the database');
+// } catch (error) {
+//     console.log('Unable to connect to the database, ', error);
+// }
+//     const port = process.env.PORT || 8000;
+
+//     const app = express();
+
+//     app.use(api);
+//     app.use(express.static(path.join(__dirname, 'presentation/web/build')));
+
+//     app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`));
+// };
 
 const port = process.env.PORT || 8000;
 
