0f9e4c6f1dc43ea999f39bc573df27f32a1aa2c2
Mateusz Król <matek8952@gmail.com>
2021-02-24 15:27:49 +0100

    SSL connection

--- src/infrastructure/postgres/DatabaseConnection.ts
+++ src/infrastructure/postgres/DatabaseConnection.ts
@@ -5,6 +5,9 @@ export const connectToDb = (url: string): Promise<Connection> => {
     return createConnection({
         type: 'postgres',
         url: url,
+        ssl: {
+            rejectUnauthorized: false,
+        },
         entities: [path.resolve(__dirname, './entity/*{.ts,.js}')],
         synchronize: true,
         logging: 'all',
