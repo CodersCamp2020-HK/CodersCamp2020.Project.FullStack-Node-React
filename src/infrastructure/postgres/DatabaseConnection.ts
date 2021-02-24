8854bfa84290e40c7cc4022244d07190ea072e1e
Mateusz Król <matek8952@gmail.com>
2021-02-24 15:39:46 +0100

    SSL connection fix

--- src/infrastructure/postgres/DatabaseConnection.ts
+++ src/infrastructure/postgres/DatabaseConnection.ts
@@ -1,13 +1,11 @@
 import { Connection, createConnection } from 'typeorm';
 import path from 'path';
 
-export const connectToDb = (url: string): Promise<Connection> => {
+export const connectToDb = (): Promise<Connection> => {
     return createConnection({
         type: 'postgres',
-        url: url,
-        ssl: {
-            rejectUnauthorized: false,
-        },
+        url: process.env.DATABASE_URL || 'postgres://root:root@localhost:5432/schronisko_dev',
+        ssl: process.env.DATABASE_URL ? true : false,
         entities: [path.resolve(__dirname, './entity/*{.ts,.js}')],
         synchronize: true,
         logging: 'all',
