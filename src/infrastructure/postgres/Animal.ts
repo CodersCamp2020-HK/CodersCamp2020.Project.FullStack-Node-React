355db48811d73122ee63a11a6977d2162876bd6d
Haivex <demozial@gmail.com>
2021-02-25 14:24:53 +0100

    Extended animal

--- src/infrastructure/postgres/Animal.ts
+++ src/infrastructure/postgres/Animal.ts
@@ -1,4 +1,10 @@
-import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+import { Entity, Column, PrimaryGeneratedColumn, OneToOne, JoinColumn } from 'typeorm';
+import { AnimalAdditionalInfo } from './AnimalAdditionalInfo';
+
+export enum AnimalSpecies {
+    CAT = 'cat',
+    DOG = 'dog',
+}
 
 @Entity()
 export class Animal {
@@ -12,4 +18,22 @@ export class Animal {
 
     @Column()
     age!: number;
+
+    @Column({
+        type: 'enum',
+        enum: AnimalSpecies,
+    })
+    specie!: AnimalSpecies;
+
+    @Column({
+        type: 'text',
+    })
+    description!: string;
+
+    @Column()
+    ready_for_adoption!: boolean;
+
+    @OneToOne(() => AnimalAdditionalInfo, { cascade: true })
+    @JoinColumn()
+    additional_info!: AnimalAdditionalInfo;
 }
