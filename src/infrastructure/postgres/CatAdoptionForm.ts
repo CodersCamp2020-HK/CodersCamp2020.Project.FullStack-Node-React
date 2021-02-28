afab10fc7fab39cff3135df55913537f3b515b08
Tomasz Dudek <t.dudeek@gmail.com>
2021-02-28 19:49:23 +0100

    Added to change branch

---
+++ src/infrastructure/postgres/CatAdoptionForm.ts
@@ -0,0 +1,43 @@
+import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class CatAddoptionForm {
+    @PrimaryGeneratedColumn()
+    id!: number;
+
+    @Column({
+        length: 50,
+    })
+    name!: string;
+
+    @Column({
+        length: 50,
+    })
+    surname!: string;
+
+    @Column()
+    birthDate!: Date;
+
+    @Column()
+    city!: string;
+
+    @Column()
+    streetName!: string;
+
+    @Column()
+    streetNumber!: number;
+
+    @Column({
+        nullable: true,
+    })
+    roomNumber!: number;
+
+    @Column()
+    cityCode!: string;
+
+    @Column()
+    telephoneNumber!: number;
+
+    @Column()
+    additionalInfo!: string;
+}
