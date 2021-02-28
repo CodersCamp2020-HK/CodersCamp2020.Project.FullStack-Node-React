6624fdc3cf829f32db9dd31d84c03d212d9a5502
Mateusz Król <matek8952@gmail.com>
2021-02-28 23:02:21 +0100

    Basic login functionality added

--- src/application/UsersService.ts
+++ src/application/UsersService.ts
@@ -1,3 +1,4 @@
+import ApiError from '@infrastructure/ApiError';
 import { User } from '@infrastructure/postgres/User';
 import { Repository } from 'typeorm';
 
@@ -7,7 +8,9 @@ export class UsersService {
     constructor(private userRepository: Repository<User>) {}
 
     public async login(userLoginParams: UserLoginParams): Promise<string> {
-        const userEmail = await this.userRepository.findOne({ where: userLoginParams.mail });
+        const user = await this.userRepository.findOne({ where: { mail: userLoginParams.mail } });
+        if (!user || user.password !== userLoginParams.password)
+            throw new ApiError('Bad Request', 400, `Wrong email and password!`);
 
         return 'JWT będzie tu';
     }
--- src/presentation/rest/UsersController.ts
+++ src/presentation/rest/UsersController.ts
@@ -1,7 +1,6 @@
-import { Body, Controller, Get, Path, Post, Query, Route, SuccessResponse, Tags } from 'tsoa';
+import { Body, Controller, Post, Route, Tags } from 'tsoa';
 import { Inject } from 'typescript-ioc';
-import { User } from '@domain/User';
-import { UserCreationParams, UsersService } from '@application/UsersService';
+import { UserLoginParams, UsersService } from '@application/UsersService';
 
 @Tags('Users')
 @Route('users')
@@ -9,16 +8,9 @@ export class UsersController extends Controller {
     @Inject
     private usersService!: UsersService;
 
-    @Get('{userId}')
-    public async getUser(@Path() userId: number, @Query() name?: string): Promise<User> {
-        return new UsersService().get(userId, name);
-    }
-
-    @SuccessResponse('201', 'Created') // Custom success response
     @Post()
-    public async createUser(@Body() requestBody: UserCreationParams): Promise<void> {
-        this.setStatus(201); // set return status 201
-        this.usersService.create(requestBody);
-        return;
+    public async loginUser(@Body() requestBody: UserLoginParams): Promise<string> {
+        this.setStatus(200);
+        return this.usersService.login(requestBody);
     }
 }
