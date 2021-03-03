37578f169fc0530bd720871f7592aecacc6958d0
Mateusz Król <matek8952@gmail.com>
2021-03-03 20:01:21 +0100

    Reinstalling packages

--- tests/example.test.ts
+++
@@ -1,9 +0,0 @@
-import { WeatherForecastApi } from '@infrastructure/WeatherForecastApi';
-
-describe('Name of the group', () => {
-    test('should ', async () => {
-        const api = new WeatherForecastApi();
-        const weather = await api.getForDate(new Date());
-        expect(weather.description).toBe('windy');
-    });
-});
