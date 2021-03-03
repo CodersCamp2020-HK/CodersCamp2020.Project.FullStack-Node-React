82e5bb40807435beb67aa1981aec84e6adc81df3
Haivex <64418079+Haivex@users.noreply.github.com>
2021-03-03 20:04:01 +0100

    Show all animals (#40)
    * Added getAll method for animals
    
    * Testing where
    
    * Extended animal
    
    * Extended animal
    
    * Added age filter
    
    * Added andWhere method and remove undefined
    
    * Added specie filter
    
    * Added readyForAdoption query
    
    * Added relation filters
    
    * Added status 404 if not found animal and refactor
    
    * Shorthand obj syntax for filters
    
    * Added OptionalWhereSelectQueryBuilder decorator
    
    * Replaced object with Record<string, unknown>
    
    * Add query params and wrap parameters into object
    
    * Added size, special diet, active level and filters
    
    Co-authored-by: Haivex <demozial@gmail.com>

--- src/application/AnimalsService.ts
+++ src/application/AnimalsService.ts
@@ -1,12 +1,46 @@
-import { Animal } from '@infrastructure/postgres/Animal';
-import { AnimalAdditionalInfo } from '@infrastructure/postgres/AnimalAdditionalInfo';
-import { Repository } from 'typeorm';
+import { Animal, AnimalSpecies } from '@infrastructure/postgres/Animal';
+import { AnimalAdditionalInfo, AnimalSize, AnimalActiveLevel } from '@infrastructure/postgres/AnimalAdditionalInfo';
+import { Repository, SelectQueryBuilder } from 'typeorm';
+import { v4 as uuidv4 } from 'uuid';
 import ApiError from '@infrastructure/ApiError';
 
 export type AnimalCreationParams = Pick<
     Animal,
     'name' | 'age' | 'specie' | 'description' | 'ready_for_adoption' | 'additional_info'
 >;
+
+interface AnimalQueryParams {
+    minAge?: number;
+    maxAge?: number;
+    specie?: AnimalSpecies;
+    readyForAdoption?: boolean;
+    temporaryHome?: boolean;
+    needDonations?: boolean;
+    virtualAdoption?: boolean;
+    acceptsKids?: boolean;
+    acceptsOtherAnimals?: boolean;
+    size?: AnimalSize;
+    activeLevel?: AnimalActiveLevel;
+}
+
+type Primitive<T> = T extends Record<string, unknown> ? never : T;
+
+class OptionalWhereSelectQueryBuilder<T> {
+    constructor(
+        public selectQueryBuilder: SelectQueryBuilder<T>,
+        private count: number = 0,
+        private uuid: string = uuidv4(),
+    ) {}
+
+    optAndWhere<P>(condition: string, param: Primitive<P>): OptionalWhereSelectQueryBuilder<T> {
+        if (param !== undefined && param !== null) {
+            const key = `${this.uuid}_${this.count++}`;
+            const query = `${condition} :${key}`;
+            this.selectQueryBuilder = this.selectQueryBuilder.andWhere(query, { [key]: param });
+        }
+        return this;
+    }
+}
 
 export class AnimalsService {
     constructor(
@@ -19,6 +53,27 @@ export class AnimalsService {
         if (!animal) throw new ApiError('Not Found', 404, 'Animal not found in database');
         //if (!animal) throw new Error('Animal not found in db');
         return animal;
+    }
+
+    public async getAll(queryParams: AnimalQueryParams): Promise<Animal[]> {
+        return new OptionalWhereSelectQueryBuilder(
+            this.animalRepository
+                .createQueryBuilder('animal')
+                .leftJoinAndSelect('animal.additional_info', 'info')
+                .where('animal.id >= :zero', { zero: 0 }),
+        )
+            .optAndWhere('animal.ready_for_adoption = ', queryParams.readyForAdoption)
+            .optAndWhere('info.temporary_home = ', queryParams.temporaryHome)
+            .optAndWhere('info.need_donations = ', queryParams.needDonations)
+            .optAndWhere('info.accepts_kids = ', queryParams.acceptsKids)
+            .optAndWhere('info.accepts_other_animals = ', queryParams.acceptsOtherAnimals)
+            .optAndWhere('info.virtual_adoption = ', queryParams.virtualAdoption)
+            .optAndWhere('info.size = ', queryParams.size)
+            .optAndWhere('info.active_level = ', queryParams.activeLevel)
+            .optAndWhere('animal.age >= ', queryParams.minAge)
+            .optAndWhere('animal.age <= ', queryParams.maxAge)
+            .optAndWhere('animal.specie = ', queryParams.specie)
+            .selectQueryBuilder.getMany();
     }
 
     public async create({
