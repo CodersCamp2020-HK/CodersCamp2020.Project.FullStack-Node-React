3c9edc92db8d252f3f8ee7677968aed7b33c519e
Haivex <demozial@gmail.com>
2021-03-04 21:07:39 +0100

    Links are stored in server and auto deleted

---
+++ src/application/TemporaryLinksStore.ts
@@ -0,0 +1,29 @@
+interface UserActivationInfo {
+    id: number;
+    email: string;
+    linkUUID: string;
+}
+export class TemporaryLinksStore {
+    constructor(deletionTimeInMinutes: number) {
+        this.deletionTimeInMinutes = deletionTimeInMinutes;
+    }
+    private deletionTimeInMinutes: number;
+    private usersActivationInfo: UserActivationInfo[] = [];
+    addLink(user: UserActivationInfo): void {
+        this.usersActivationInfo.push(user);
+
+        setTimeout(() => {
+            if (this.usersActivationInfo.find((userToFind) => userToFind.linkUUID == user.linkUUID)) {
+                this.deleteLink(user);
+            }
+        }, this.deletionTimeInMinutes * 1000 * 60);
+    }
+    deleteLink(user: UserActivationInfo): void {
+        this.usersActivationInfo = this.usersActivationInfo.filter((userToDelete) => {
+            userToDelete.linkUUID != user.linkUUID;
+        });
+    }
+    getAllLinks(): UserActivationInfo[] {
+        return this.usersActivationInfo;
+    }
+}
--- src/presentation/rest/UsersController.ts
+++ src/presentation/rest/UsersController.ts
@@ -1,14 +1,14 @@
 import { Controller, Path, Post, Route, SuccessResponse, Tags } from 'tsoa';
 import { Inject } from 'typescript-ioc';
 import { UsersService } from '@application/UsersService';
+import { TemporaryLinksStore } from '@application/TemporaryLinksStore';
 
-const sampleUsers = [
-    {
-        id: 1,
-        email: 'asd@asd.asd',
-        UUID: 'secretuuid',
-    },
-];
+const linksStore = new TemporaryLinksStore(1);
+linksStore.addLink({
+    id: 1,
+    email: 'asd@asd.asd',
+    linkUUID: 'secretuuid',
+});
 
 @Tags('Users')
 @Route('users')
@@ -19,15 +19,16 @@ export class UsersController extends Controller {
     @Post('activate/{generatedUUID}')
     @SuccessResponse('200', 'User Activated')
     public async sendEmail(@Path() generatedUUID: string): Promise<void> {
-        const foundedUser = sampleUsers.find((el) => {
-            return el.UUID == generatedUUID;
+        const foundedUser = linksStore.getAllLinks().find((el) => {
+            return el.linkUUID == generatedUUID;
         });
 
         if (foundedUser) {
             await this.usersService.activateUser(foundedUser.id);
+            linksStore.deleteLink(foundedUser);
             this.setStatus(200);
         } else {
-            throw new Error('User not found');
+            throw new Error('Link is not valid or expired');
         }
         return;
     }
