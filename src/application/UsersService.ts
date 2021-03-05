5e2027718c9aa886e776e90f0f4b3a5b0bddfa7b
Weronika Dziedzic <wera02@gmail.com>
2021-03-05 13:05:31 +0100

    save updateUser

--- src/application/UsersService.ts
+++ src/application/UsersService.ts
@@ -2,13 +2,15 @@ import { User } from '@infrastructure/postgres/User';
 import ApiError from '@infrastructure/ApiError';
 import { Repository } from 'typeorm';
 
-export type UserCreationParams = Pick<User, 'email' | 'name' | 'phoneNumbers'>;
-export type UserUpdateParams = Partial<UserCreationParams>;
+export type UserUpdateParams = Pick<User, 'name' | 'phone' | 'surname'>;
 export class UsersService {
     constructor(private userRepository: Repository<User>) {}
 
-    public update(id: number, { ...userParams }: UserUpdateParams): Promise<User> {
+    public async update(id: number, userUpdateParams: UserUpdateParams): Promise<User> {
         const user = await this.userRepository.findOne(id);
         if (!user) throw new ApiError('Not Found', 404, `User with id: ${id} not found!`);
+        const updateUser = { ...user, ...userUpdateParams };
+
+        return this.userRepository.save(updateUser);
     }
 }
