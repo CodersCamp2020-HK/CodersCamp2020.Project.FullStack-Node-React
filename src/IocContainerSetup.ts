60145f709dabe5dd07c9524226bb9d2cb7d48a77
Weronika Dziedzic <wera02@gmail.com>
2021-03-07 17:53:21 +0100

    Resolving conflicts

--- src/IocContainerSetup.ts
+++ src/IocContainerSetup.ts
@@ -8,8 +8,8 @@ import { User } from '@infrastructure/postgres/User';
 import { AnimalAdditionalInfo } from '@infrastructure/postgres/AnimalAdditionalInfo';
 import { QuestionnaireService } from '@application/QuestionnaireService';
 import { Questionnaire } from '@infrastructure/postgres/Questionnaire';
-import { UsersService } from '@application/UsersService';
-import { User } from '@infrastructure/postgres/User';
+// import { UsersService } from '@application/UsersService';
+// import { User } from '@infrastructure/postgres/User';
 
 Container.bind(AnimalsService)
     .factory(
@@ -25,4 +25,3 @@ Container.bind(UsersService).factory(() => new UsersService(getConnection().getR
 Container.bind(QuestionnaireService)
     .factory(() => new QuestionnaireService(getConnection().getRepository(Questionnaire)))
     .scope(Scope.Local);
-Container.bind(UsersService).factory(() => new UsersService(getConnection().getRepository(User)));
