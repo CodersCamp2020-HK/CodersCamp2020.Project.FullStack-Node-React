4302ba92ff70dcb7c0ee321c6bf46b0c2660a457
Mateusz Król <matek8952@gmail.com>
2021-03-10 15:33:46 +0100

    Auth role chagned

--- src/infrastructure/Auth.ts
+++ src/infrastructure/Auth.ts
@@ -2,6 +2,7 @@ import { Request } from 'express';
 import jwt, { JsonWebTokenError } from 'jsonwebtoken';
 import { getConnection } from 'typeorm';
 import ApiError from './ApiError';
+import OrganizationUser from './postgres/OrganizationUser';
 import { User, UserType } from './postgres/User';
 
 export interface IUserInfo {
@@ -32,14 +33,20 @@ export async function expressAuthentication(
             const token = request.header('access_token');
             if (!token) throw new ApiError('Bad Request', 400, 'No token provided');
             if (!process.env.JWT_KEY) throw new ApiError('Internal server error', 500, 'JWT private key not found!');
+
             const decoded = jwt.verify(token, process.env.JWT_KEY);
             if (!isUserInfo(decoded)) throw new ApiError('Bad Request', 400, 'Invalid Token');
             if (scopes && !scopes.includes(decoded.role))
                 throw new ApiError('Unathorized', 401, 'Authorization failed');
-            const repository = getConnection().getRepository(User);
-            const user = await repository.findOne(decoded.id);
+
+            const userRepository = getConnection().getRepository(User);
+            const user = await userRepository.findOne(decoded.id);
             if (!user) throw new ApiError('Unauthorized', 401, 'This user does not exist');
-            if (user.type !== decoded.role) throw new ApiError('Bad Request', 400, 'Invalid token');
+
+            const organizationUserRepository = getConnection().getRepository(OrganizationUser);
+            const organizationUser = await organizationUserRepository.findOne(user.id);
+            const role = organizationUser === undefined ? UserType.NORMAL : organizationUser.role;
+            if (role !== decoded.role) throw new ApiError('Bad Request', 400, 'Invalid token');
             return decoded;
         } catch (error) {
             if (error instanceof JsonWebTokenError) throw new ApiError('Bad Request', 400, 'Invalid token');
