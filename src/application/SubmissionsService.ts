16c61cba7c643f02b112b17e7aed2b4bd2f94386
Mateusz Król <matek8952@gmail.com>
2021-03-11 19:05:53 +0100

    Added submission service

---
+++ src/application/SubmissionsService.ts
@@ -0,0 +1,27 @@
+import FormAnimalSubmission from '@infrastructure/postgres/FormAnimalSubmission';
+import { Repository } from 'typeorm';
+import OptionalWhereSelectQueryBuilder from 'utils/OptionalWhereSelectQueryBuilder';
+
+export enum FormStatus {
+    IN_PROGRESS = 'in progress',
+    REJECTED = 'rejected',
+    ACCEPTED = 'accepted',
+}
+
+interface getAllAnimalSubmissionsParams {
+    date: Date;
+    specie: string;
+    status: FormStatus;
+}
+
+export class SubmissionsService {
+    constructor(private animalSubmissionRepository: Repository<FormAnimalSubmission>) {}
+
+    public async getAllAnimalSubmissions(queryParams: getAllAnimalSubmissionsParams): Promise<FormAnimalSubmission[]> {
+        return new OptionalWhereSelectQueryBuilder(this.animalSubmissionRepository.createQueryBuilder('submission'))
+            .optAndWhere('submission.status = ', queryParams.status)
+            .optAndWhere('submission.status = ', queryParams.date)
+            .optAndWhere('submission.status = ', queryParams.specie)
+            .selectQueryBuilder.getMany();
+    }
+}
