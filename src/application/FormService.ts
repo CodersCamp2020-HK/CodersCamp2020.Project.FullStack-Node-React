4f455c435c3aced6f0341a579ac723206b46078d
Haivex <demozial@gmail.com>
2021-03-12 20:09:31 +0100

    Working volunteer repository

--- src/application/FormService.ts
+++ src/application/FormService.ts
@@ -51,23 +51,19 @@ export class FormService {
     }
 
     public async changeStatusForVolunteerForm(changeStatusParams: ChangeStatusForVolunterFormParams): Promise<void> {
-        console.log(this.volunteerSubmissionRepository);
-        console.log(changeStatusParams);
-        // await this.volunteerSubmissionRepository
-        //     .createQueryBuilder()
-        //     .update(FormVolunteerSubmission)
-        //     .set({ status: changeStatusParams.status })
-        //     .where('userId = :id', { id: changeStatusParams.userId })
-        //     .execute();
+        await this.volunteerSubmissionRepository
+            .createQueryBuilder()
+            .update()
+            .set({ status: changeStatusParams.status })
+            .where('userId = :id', { id: changeStatusParams.userId })
+            .execute();
         return;
     }
 
     public async changeStatusForAdoptionForm(changeStatusParams: ChangeStatusForAdoptionFormParams): Promise<void> {
-        console.log(this.animalSubmissionRepository);
-        console.log(changeStatusParams);
         await this.animalSubmissionRepository
             .createQueryBuilder()
-            .update(FormAnimalSubmission)
+            .update()
             .set({ status: changeStatusParams.status })
             .where('applicantId = :id', { id: changeStatusParams.userId })
             .andWhere('animalId = :id', { id: changeStatusParams.animalId })
