48f526eadd37909dd9831b110c424c2e89b35f67
Mateusz Król <matek8952@gmail.com>
2021-03-13 20:01:50 +0100

    Submission join

--- src/application/VolunteerSubmissionsService.ts
+++ src/application/VolunteerSubmissionsService.ts
@@ -1,5 +1,7 @@
+import ApiError from '@infrastructure/ApiError';
 import FormVolunteerSubmission, { VolunteerFormStatus } from '@infrastructure/postgres/FormVolunteerSubmission';
 import { Repository } from 'typeorm';
+import OptionalWhereSelectQueryBuilder from 'utils/OptionalWhereSelectQueryBuilder';
 
 export interface ChangeStatusForVolunterFormParams {
     status: VolunteerFormStatus;
@@ -17,5 +19,19 @@ export class VolunteerSubmissionsService {
             .where('userId = :id', { id: changeStatusParams.userId })
             .execute();
         return;
+    }
+
+    public async getAllSubmissions(): Promise<FormVolunteerSubmission[]> {
+        const submissions = await new OptionalWhereSelectQueryBuilder(
+            this.volunteerSubmissionRepository
+                .createQueryBuilder('submission')
+                .leftJoinAndSelect('submission.user', 'user')
+                .leftJoinAndSelect('submission.reviewer', 'reviewer')
+                .leftJoinAndSelect('submission.answers', 'answers'),
+        ).selectQueryBuilder.getMany();
+
+        if (submissions.length === 0) throw new ApiError('Not Found', 404, 'Submissions not found');
+
+        return submissions;
     }
 }
