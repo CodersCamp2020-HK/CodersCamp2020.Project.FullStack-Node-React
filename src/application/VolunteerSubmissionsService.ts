7bed19f4df6c37995b3a4b4f90d3e04ce119f77b
Mateusz Król <37661247+KrolMateusz@users.noreply.github.com>
2021-03-13 22:57:18 +0100

    ENDPOINT | Wszystkie formularze na wolontariusza (#108)
    * Submission join
    
    * Query string params
    
    * Volunteer controller

--- src/application/VolunteerSubmissionsService.ts
+++ src/application/VolunteerSubmissionsService.ts
@@ -1,9 +1,18 @@
+import ApiError from '@infrastructure/ApiError';
 import FormVolunteerSubmission, { VolunteerFormStatus } from '@infrastructure/postgres/FormVolunteerSubmission';
 import { Repository } from 'typeorm';
+import OptionalWhereSelectQueryBuilder from 'utils/OptionalWhereSelectQueryBuilder';
 
 export interface ChangeStatusForVolunterFormParams {
     status: VolunteerFormStatus;
     userId: number;
+}
+
+interface SubmissionQueryParams {
+    submissionDate?: Date;
+    status?: VolunteerFormStatus;
+    userName?: string;
+    reviewerName?: string;
 }
 
 export class VolunteerSubmissionsService {
@@ -17,5 +26,24 @@ export class VolunteerSubmissionsService {
             .where('userId = :id', { id: changeStatusParams.userId })
             .execute();
         return;
+    }
+
+    public async getAllSubmissions(queryParams: SubmissionQueryParams): Promise<FormVolunteerSubmission[]> {
+        const submissions = await new OptionalWhereSelectQueryBuilder(
+            this.volunteerSubmissionRepository
+                .createQueryBuilder('submission')
+                .leftJoinAndSelect('submission.user', 'user')
+                .leftJoinAndSelect('submission.reviewer', 'reviewer')
+                .leftJoinAndSelect('submission.answers', 'answers'),
+        )
+            .optAndWhere('submission.status = ', queryParams.status)
+            .optAndWhere('submission.submissionDate = ', queryParams.submissionDate)
+            .optAndWhere('user.name = ', queryParams.userName)
+            .optAndWhere('reviewer.name = ', queryParams.reviewerName)
+            .selectQueryBuilder.getMany();
+
+        if (submissions.length === 0) throw new ApiError('Not Found', 404, 'Submissions not found');
+
+        return submissions;
     }
 }
