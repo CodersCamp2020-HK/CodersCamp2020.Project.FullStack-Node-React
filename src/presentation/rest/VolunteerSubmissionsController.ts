0e540414de05edc6617fca7c57249d677c32b510
Mateusz Król <matek8952@gmail.com>
2021-03-14 18:16:26 +0100

    Volunteer controller added

--- src/presentation/rest/VolunteerSubmissionsController.ts
+++ src/presentation/rest/VolunteerSubmissionsController.ts
@@ -1,10 +1,12 @@
 import {
     ChangeStatusForVolunterFormParams,
+    PostVolunteerSubmissionParams,
     VolunteerSubmissionsService,
 } from '@application/VolunteerSubmissionsService';
 import ApiError from '@infrastructure/ApiError';
+import { IAuthUserInfoRequest } from '@infrastructure/Auth';
 import FormVolunteerSubmission, { VolunteerFormStatus } from '@infrastructure/postgres/FormVolunteerSubmission';
-import { Body, Get, Path, Put, Query, Route, Tags, Response } from 'tsoa';
+import { Body, Get, Path, Put, Query, Route, Tags, Response, Security, SuccessResponse, Post, Request } from 'tsoa';
 import { Inject } from 'typescript-ioc';
 
 @Tags('Volunteer Submissions')
@@ -39,5 +41,15 @@ export class VolunteerSubmissionsController {
     @Get('{id}')
     public async getVolunteerSubmission(@Path() id: number): Promise<FormVolunteerSubmission> {
         return this.submissionService.getVolunteerSubmission(id);
+    }
+
+    @Security('jwt', ['normal'])
+    @SuccessResponse(201, 'Created')
+    @Post('add')
+    public async postVolunteerSubmission(
+        @Body() requestBody: PostVolunteerSubmissionParams,
+        @Request() request: IAuthUserInfoRequest,
+    ): Promise<void> {
+        this.submissionService.createVolunteerSubmission(requestBody, request);
     }
 }
