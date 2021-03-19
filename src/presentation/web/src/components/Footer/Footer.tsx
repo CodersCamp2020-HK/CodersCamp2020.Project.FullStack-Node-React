b2e2a0b9ea5ac1b7e3eaff07c2f2981776cb9c32
Haivex <demozial@gmail.com>
2021-03-19 19:50:24 +0100

    Added footer, color to themes and typography

--- src/presentation/web/src/components/Footer/Footer.tsx
+++ src/presentation/web/src/components/Footer/Footer.tsx
@@ -1,20 +1,21 @@
 import React from 'react';
 import styles from './Footer.module.css';
+import { Typography } from '@material-ui/core';
 
 const Footer = () => {
     return (
             <footer className={styles.footer}>
                 <div className={styles.footerColumn}>
-                    <span>Telefon:</span>
-                    <span>111-222-333</span>
+                    <Typography variant='subtitle2'>Telefon:</Typography>
+                    <Typography variant='subtitle2'>111-222-333</Typography>
                 </div>
                 <div className={styles.footerColumn}>
-                    <span>E-mail:</span>
-                    <span>schronisko@schronisko-zlapki.com</span>
+                    <Typography variant='subtitle2'>E-mail:</Typography>
+                    <Typography variant='subtitle2'>schronisko@schronisko-zlapki.com</Typography>
                 </div>
                 <div className={styles.footerColumn}>
-                    <span>Adres: ul. Pieska 10</span>
-                    <span>00-000 Psary</span>
+                    <Typography variant='subtitle2'>Adres: ul. Pieska 10</Typography>
+                    <Typography variant='subtitle2'>00-000 Psary</Typography>
                 </div>
             </footer>
     );
--- src/presentation/web/src/themes/theme.tsx
+++ src/presentation/web/src/themes/theme.tsx
@@ -12,6 +12,12 @@ const theme = createMuiTheme({
             main: '#827717',
             dark: '#524C00',
         },
+        common: {
+            white: '#FFFDF6'
+        },
+        text: {
+            secondary: 'rgba(0, 0, 0, 0.54)'
+        },
     },
     props: {
         MuiLink: {
@@ -32,5 +38,17 @@ const theme = createMuiTheme({
         },
     },
 });
+
+// typography {
+//     subtitle2: {
+//         fontFamily: [
+//             'Roboto'
+//         ],
+//         fontSize: 14,
+//         fontStyle: 'normal',
+//         fontWeight: 500,
+        
+//     }
+// },
 
 export default theme;
