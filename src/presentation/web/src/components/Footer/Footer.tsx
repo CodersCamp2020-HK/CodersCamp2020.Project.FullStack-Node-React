1df50be20926a047949db8384aaea19ab0931c4a
Haivex <demozial@gmail.com>
2021-03-19 20:51:50 +0100

    Added right padding

--- src/presentation/web/src/components/Footer/Footer.tsx
+++ src/presentation/web/src/components/Footer/Footer.tsx
@@ -1,26 +1,24 @@
+import { Box, Typography } from '@material-ui/core';
+import { useTheme } from '@material-ui/styles';
 import React from 'react';
 import styles from './Footer.module.css';
-import { Typography, Box } from '@material-ui/core';
-import { useTheme } from '@material-ui/styles';
 
 const Footer = () => {
     const theme = useTheme();
     return (
-            <Box boxShadow={8} p={18} bgcolor={ theme.common.white }>
-            <footer className={styles.footer}>
+            <Box width='100%' boxShadow={8} paddingTop={9} paddingBottom={9} className={styles.footer}>
                 <div className={styles.footerColumn}>
                     <Typography color='textSecondary' variant='subtitle2'>Telefon:</Typography>
                     <Typography color='textSecondary' variant='subtitle2'>111-222-333</Typography>
                 </div>
                 <div className={styles.footerColumn}>
                     <Typography color='textSecondary' variant='subtitle2'>E-mail:</Typography>
-                    <Typography variant='subtitle2'>schronisko@schronisko-zlapki.com</Typography>
+                    <Typography color='textSecondary' variant='subtitle2'>schronisko@schronisko-zlapki.com</Typography>
                 </div>
                 <div className={styles.footerColumn}>
                     <Typography color='textSecondary' variant='subtitle2'>Adres: ul. Pieska 10</Typography>
                     <Typography color='textSecondary' variant='subtitle2'>00-000 Psary</Typography>
                 </div>
-            </footer>
             </Box>
     );
 };
