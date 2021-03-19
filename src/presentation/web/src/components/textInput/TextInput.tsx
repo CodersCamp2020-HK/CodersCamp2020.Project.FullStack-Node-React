5bf34def40e92379b06b41e08c391d2d8e5304ca
Mateusz Król <matek8952@gmail.com>
2021-03-19 19:02:47 +0100

    Added color

--- src/presentation/web/src/components/textInput/TextInput.tsx
+++ src/presentation/web/src/components/textInput/TextInput.tsx
@@ -8,6 +8,7 @@ interface TextInputProps {
     variant?: 'filled' | 'outlined' | 'standard';
     size?: 'medium' | 'small';
     required?: boolean;
+    color?: 'primary' | 'secondary';
 }
 
 const TextInput = ({
@@ -17,8 +18,11 @@ const TextInput = ({
     variant = 'outlined',
     size = 'medium',
     required = false,
+    color = 'primary',
 }: TextInputProps) => {
-    return <TextField id={id} label={label} type={type} variant={variant} size={size} required={required} />;
+    return (
+        <TextField id={id} label={label} type={type} variant={variant} size={size} required={required} color={color} />
+    );
 };
 
 export default TextInput;
