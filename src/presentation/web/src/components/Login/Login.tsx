0df4a087918785e83f16d3cfe75e48bacc617e21
Haivex <demozial@gmail.com>
2021-03-20 17:49:05 +0100

    Added inputs and validation style

--- src/presentation/web/src/components/Login/Login.tsx
+++ src/presentation/web/src/components/Login/Login.tsx
@@ -1,10 +1,11 @@
-import { Avatar, Paper, Theme, useTheme } from '@material-ui/core';
+import { Avatar, Paper, TextField, Theme, Typography, useTheme } from '@material-ui/core';
 import { LockOutlined } from '@material-ui/icons';
 import { makeStyles } from '@material-ui/styles';
-import React from 'react';
+import React, { useState } from 'react';
 
 const Login = () => {
     const theme = useTheme<Theme>();
+    const [isErrors, setErrors] = useState<boolean>(false);
     const useStyle = makeStyles({
         greenBackground: {
             backgroundColor: theme.palette.secondary.dark,
@@ -14,10 +15,13 @@ const Login = () => {
     const classes = useStyle();
 
     return (
-        <Paper variant="outlined" square={false}>
+        <Paper variant="outlined" square={false} >
             <Avatar variant="circle" className={classes.greenBackground}>
                 <LockOutlined htmlColor={theme.palette.secondary.contrastText} />
             </Avatar>
+            <Typography variant='h5'>Zaloguj się</Typography>
+            <TextField label='E-mail' type='email' variant='outlined' size='medium' required error={isErrors} autoFocus />
+            <TextField label='Password' type='password' variant='outlined' size='medium' required error={isErrors} helperText={isErrors && 'Nieprawidłowy e-mail lub hasło'}/>
         </Paper>
     );
 };
