d7bd0002791109ac40429e87661c213d91f2fbe4
Haivex <demozial@gmail.com>
2021-03-20 17:13:21 +0100

    Fixed lock color

--- src/presentation/web/src/components/Login/Login.tsx
+++ src/presentation/web/src/components/Login/Login.tsx
@@ -1,12 +1,21 @@
 import { Avatar, Paper, Theme, useTheme } from '@material-ui/core';
 import { LockOutlined } from '@material-ui/icons';
+import { makeStyles } from '@material-ui/styles';
 import React from 'react';
 
 const Login = () => {
-    const theme = useTheme<Theme>()
+    const theme = useTheme<Theme>();
+    const useStyle = makeStyles({
+        greenBackground: {
+            backgroundColor: theme.palette.secondary.dark,
+        },
+    })
+
+    const classes = useStyle();
+
     return (
-        <Paper variant='outlined' square={false}>
-            <Avatar variant='circle' color={theme.palette.secondary.dark}>
+        <Paper variant="outlined" square={false}>
+            <Avatar variant="circle" className={classes.greenBackground}>
                 <LockOutlined htmlColor={theme.palette.secondary.contrastText} />
             </Avatar>
         </Paper>
