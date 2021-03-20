6d05e4969448e28d4f01602ca6c1d1becff2a535
Mateusz Król <matek8952@gmail.com>
2021-03-20 16:52:34 +0100

    Generic text field

---
+++ src/presentation/web/src/components/textInput/TextInput.tsx
@@ -0,0 +1,54 @@
+import React from 'react';
+import TextField from '@material-ui/core/TextField';
+import InputAdornment from '@material-ui/core/InputAdornment';
+import Today from '@material-ui/icons/Today';
+import styles from './TextInput.module.css';
+import { FieldElement } from 'react-hook-form';
+
+interface TextInputProps {
+    name: string;
+    label: string;
+    type?: string;
+    variant?: 'filled' | 'outlined' | 'standard';
+    size?: 'medium' | 'small';
+    required?: boolean;
+    color?: 'primary' | 'secondary';
+    ref?: FieldElement
+}
+
+const TextInput = ({
+    name,
+    label,
+    type = 'text',
+    variant = 'outlined',
+    size = 'medium',
+    required = false,
+    color = 'secondary',
+    ref
+}: TextInputProps) => {
+    const endAndorment =
+        type === 'date'
+            ? {
+                  endAdornment: (
+                      <InputAdornment position="end">
+                          <Today className={styles.calendarIcon} />
+                      </InputAdornment>
+                  ),
+              }
+            : undefined;
+    return (
+        <TextField
+            name={name}
+            label={label}
+            type={type}
+            variant={variant}
+            size={size}
+            required={required}
+            color={color}
+            InputProps={endAndorment}
+            ref={ref}
+        />
+    );
+};
+
+export default TextInput;
