eb917c0483ae3db4454255fba3f5ee7deea61ee9
Haivex <demozial@gmail.com>
2021-03-21 19:34:50 +0100

    Size icon

--- src/presentation/web/src/components/activationSent/ActivationSent.tsx
+++ src/presentation/web/src/components/activationSent/ActivationSent.tsx
@@ -1,13 +1,15 @@
 import { Avatar, Grid, Link, Paper, Theme, Typography } from '@material-ui/core';
 import { makeStyles, useTheme } from '@material-ui/styles';
 import { FaEnvelopeOpenText } from 'react-icons/fa';
-import { Link as RouterLink } from 'react-router-dom'
+import { Link as RouterLink } from 'react-router-dom';
 
 const ActivationSent = () => {
     const theme = useTheme<Theme>();
     const useStyle = makeStyles({
         greenBackground: {
             backgroundColor: theme.palette.secondary.dark,
+            width: '200px',
+            height: '200px',
         },
         paper: {
             padding: theme.spacing(2),
@@ -18,17 +20,21 @@ const ActivationSent = () => {
         returnToLogin: {
             color: theme.palette.info.dark,
         },
+        icon: {
+            fontSize: 72,
+        },
     });
-    const classes = useStyle();  
+    const classes = useStyle();
     return (
         <Grid spacing={0} container direction="column" item xs={4}>
             <Grid item xs>
                 <Paper className={classes.paper} variant="outlined" square={false}>
                     <Avatar variant="circular" className={classes.greenBackground}>
-                        <FaEnvelopeOpenText color={theme.palette.secondary.contrastText} />
+                        <FaEnvelopeOpenText size="100" color={theme.palette.secondary.contrastText} />
                     </Avatar>
-                    <Typography variant='body1'>
-                    Sprawdź swoją skrzynkę mailową. Mail z linkiem aktywacyjnym powienien zostać dostarczony w przeciągu kilku minut.
+                    <Typography variant="body1">
+                        Sprawdź swoją skrzynkę mailową. Mail z linkiem aktywacyjnym powienien zostać dostarczony w
+                        przeciągu kilku minut.
                     </Typography>
                     <Link component={RouterLink} className={classes.returnToLogin} to="/login">
                         <Typography variant="body2">Wróć do logowania</Typography>
@@ -36,7 +42,7 @@ const ActivationSent = () => {
                 </Paper>
             </Grid>
         </Grid>
-    )
-}
+    );
+};
 
 export default ActivationSent;
