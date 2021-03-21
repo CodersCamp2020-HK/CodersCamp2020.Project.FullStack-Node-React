615489bf6b628bddc9427af4546670d36f9a5c01
Haivex <demozial@gmail.com>
2021-03-21 13:07:12 +0100

    Change folders names

--- src/presentation/web/src/App.tsx
+++ src/presentation/web/src/App.tsx
@@ -2,13 +2,13 @@ import { Container, ThemeProvider } from '@material-ui/core';
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import './App.css';
-import Login from './components/Login/Login';
-import Navbar from './components/Navbar/Navbar';
-import About from './components/Pages/About';
-import Adoption from './components/Pages/Adoption';
-import Contact from './components/Pages/Contact';
-import Donation from './components/Pages/Donation';
-import Home from './components/Pages/Home';
+import Login from './components/login/Login';
+import Navbar from './components/navbar/Navbar';
+import About from './components/pages/About';
+import Adoption from './components/pages/Adoption';
+import Contact from './components/pages/Contact';
+import Donation from './components/pages/Donation';
+import Home from './components/pages/Home';
 import theme from './themes/theme';
 
 const App: React.FC = () => {
