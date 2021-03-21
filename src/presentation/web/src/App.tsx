eadd18c6b77b94e3156570de5fad32dbcd01d09b
bat098 <75204583+bat098@users.noreply.github.com>
2021-03-21 16:32:55 +0100

    Rename folders (#170)
    * update master
    
    * rename folders
    
    * rename folders
    
    * rename folders

--- src/presentation/web/src/App.tsx
+++ src/presentation/web/src/App.tsx
@@ -4,12 +4,12 @@ import theme from './themes/theme';
 import { Container, ThemeProvider } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
 import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
-import About from './components/Pages/About';
-import Contact from './components/Pages/Contact';
-import Home from './components/Pages/Home';
-import Donation from './components/Pages/Donation';
-import Adoption from './components/Pages/Adoption';
-import Navbar from './components/Navbar/Navbar';
+import About from './components/pages1/About';
+import Contact from './components/pages1/Contact';
+import Home from './components/pages1/Home';
+import Donation from './components/pages1/Donation';
+import Adoption from './components/pages1/Adoption';
+import Navbar from './components/navbar1/Navbar';
 
 const App: React.FC = () => {
     return (
