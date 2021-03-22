723c5499f4138ea043e9888d13021b9384c0912d
Haivex <demozial@gmail.com>
2021-03-22 19:40:11 +0100

    Added card image

--- src/presentation/web/src/components/slider/Slider.tsx
+++ src/presentation/web/src/components/slider/Slider.tsx
@@ -1,33 +1,47 @@
-import React from 'react';
-import { Slide, Card, CardMedia, IconButton, makeStyles, useTheme, Theme } from '@material-ui/core';
+import { Card, CardMedia, Container, IconButton, makeStyles, Theme, useTheme } from '@material-ui/core';
 import { ArrowBack, ArrowForward } from '@material-ui/icons';
+import React from 'react';
 
 interface SliderProps {
-    photos: string[]
+    photos: string[];
 }
 
-const Slider = ({photos}: SliderProps) => {
-    const theme = useTheme<Theme>()
+const Slider = ({ photos }: SliderProps) => {
+    const theme = useTheme<Theme>();
     const useStyle = makeStyles({
         buttonBackground: {
             backgroundColor: theme.palette.secondary.main,
             color: theme.palette.secondary.contrastText,
             '&:hover': {
                 backgroundColor: theme.palette.secondary.light,
-        }
-    }
-    })
+            },
+        },
+        card: {
+            maxWidth: '50%',
+        },
+        image: {
+            width: '100%',
+            height: '100%',
+        },
+    });
     const classes = useStyle();
     return (
-        <Card>
+        <Container>
+            <Card className={classes.card}>
+                <CardMedia
+                    className={classes.image}
+                    component="img"
+                    image="https://images.pexels.com/photos/2817421/pexels-photo-2817421.jpeg"
+                />
+            </Card>
             <IconButton className={classes.buttonBackground}>
                 <ArrowBack />
             </IconButton>
             <IconButton className={classes.buttonBackground}>
                 <ArrowForward />
             </IconButton>
-        </Card>
-    )
-}
+        </Container>
+    );
+};
 
 export default Slider;
