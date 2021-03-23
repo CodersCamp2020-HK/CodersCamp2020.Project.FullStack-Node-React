ba9925d0eaa8db3a327990e9e8b01c387ac74fd4
Mateusz Król <matek8952@gmail.com>
2021-03-23 17:31:49 +0100

    AdoptionStep

--- src/infrastructure/postgres/dummyData/adoptionSteps.ts
+++ src/infrastructure/postgres/dummyData/adoptionSteps.ts
@@ -11,6 +11,28 @@ export const seedAdoptionStep = (): DeepPartial<AdoptionStep>[] => {
             specie: { id: 1 },
             form: { id: 1 },
         },
+        {
+            name: 'Krok 2 - spotkanie w schronisku',
+            description: 'Jednorazowe spotkanie adoptującego z psem w schronisku',
+            number: 2,
+            organization: { id: 1 },
+            specie: { id: 1 },
+        },
+        {
+            name: 'Krok 1 - wypełnienie formularza',
+            description: 'Krok zawierający formularz adopcyjny dla kota. Wymagany do adopcji',
+            number: 1,
+            organization: { id: 1 },
+            specie: { id: 2 },
+            form: { id: 2 },
+        },
+        {
+            name: 'Krok 2 - spotkanie w schronisku',
+            description: 'Jednorazowe spotkanie adoptującego z kotem w schronisku',
+            number: 2,
+            organization: { id: 1 },
+            specie: { id: 2 },
+        },
     ];
     return adoptionStep;
 };
