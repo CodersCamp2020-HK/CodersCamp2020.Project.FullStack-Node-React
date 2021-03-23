974f62ad6ee501b083676db07bc23509f53a6887
Haivex <demozial@gmail.com>
2021-03-23 16:14:53 +0100

    Removed route for tests

--- src/presentation/web/src/App.tsx
+++ src/presentation/web/src/App.tsx
@@ -9,8 +9,6 @@ import Adoption from './components/pages/Adoption';
 import Contact from './components/pages/Contact';
 import Donation from './components/pages/Donation';
 import Home from './components/pages/Home';
-import Slider from './components/slider/Slider';
-import { photos } from './components/slider/mockImages';
 import theme from './themes/theme';
 
 const App: React.FC = () => {
@@ -34,9 +32,6 @@ const App: React.FC = () => {
                         </Route>
                         <Route exact path="/contact">
                             <Contact />
-                        </Route>
-                        <Route exact path="/testing">
-                            <Slider photos={photos} />
                         </Route>
                     </Switch>
                 </Router>
