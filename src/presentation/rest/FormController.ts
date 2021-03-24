c04412dc880ff67cb204895c2fafdc98fa8ac4ab
Mateusz Król <matek8952@gmail.com>
2021-03-24 15:56:48 +0100

    Get form by animal id

--- src/presentation/rest/FormController.ts
+++ src/presentation/rest/FormController.ts
@@ -30,16 +30,16 @@ export class FormController extends Controller {
     }
 
     /**
-     * Supply an ID of survey and get it from database
-     * @param surveyId ID of survey (number)
+     * Supply an ID of animal and get it from database
+     * @param animalId ID of animal (number)
      */
     @Security('jwt', ['normal', 'volunteer', 'admin', 'employee'])
     @Response<ApiError>(404, 'Survey not found')
     @Response<Error>(500, 'Internal Server Error')
     @SuccessResponse(200, 'ok')
-    @Get('{surveyId}')
-    public async getForm(@Path() surveyId: number): Promise<AdoptionStep> {
-        return this.formService.get(surveyId);
+    @Get('{animalId}')
+    public async getForm(@Path() animalId: number): Promise<AdoptionStep> {
+        return this.formService.get(animalId);
     }
 
     /**
