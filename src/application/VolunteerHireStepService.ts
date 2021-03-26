a6f9c5fa243f6ddc124bd1a7e02aa34a28fdb5f3
Haivex <demozial@gmail.com>
2021-03-26 18:14:40 +0100

    Added get all steps

--- src/application/VolunteerHireStepService.ts
+++ src/application/VolunteerHireStepService.ts
@@ -5,7 +5,15 @@ import VolunteerHireStep from '@infrastructure/postgres/VolunteerHireStep';
 export class VolunteerHireStepService {
     constructor(private volunteerHireStepRepository: Repository<VolunteerHireStep>) {}
 
-    public async get(userId: number): Promise<VolunteerHireStep> {
-        
+    public async getAll(): Promise<VolunteerHireStep[]> {
+        const volunteerSteps = await this.volunteerHireStepRepository.find({
+            where: {
+                organization: {
+                    id: 1,
+                },
+            },
+        });
+        if (!volunteerSteps) throw new ApiError('Not found', 404, 'Volunteer steps not found');
+        return volunteerSteps;
     }
 }
