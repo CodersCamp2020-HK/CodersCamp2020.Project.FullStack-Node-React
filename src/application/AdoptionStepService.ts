cdc665d9487957fdc419d7c0d54b7ff9d5eeb939
Haivex <demozial@gmail.com>
2021-03-26 19:07:05 +0100

    Added get all

---
+++ src/application/AdoptionStepService.ts
@@ -0,0 +1,19 @@
+import { Repository } from 'typeorm';
+import ApiError from '@infrastructure/ApiError';
+import AdoptionStep from '@infrastructure/postgres/AdoptionStep';
+
+export class AdoptionStepService {
+    constructor(private AdoptionStepRepository: Repository<AdoptionStep>) {}
+
+    public async getAll(): Promise<AdoptionStep[]> {
+        const adoptionSteps = await this.AdoptionStepRepository.find({
+            where: {
+                organization: {
+                    id: 1,
+                },
+            },
+        });
+        if (!adoptionSteps) throw new ApiError('Not found', 404, 'Adoption steps not found');
+        return adoptionSteps;
+    }
+}
