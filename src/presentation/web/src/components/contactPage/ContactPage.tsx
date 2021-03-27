b251df199db5f6c9d88f94e2a1a3e4d7ebf67531
Tomasz Dudek <t.dudeek@gmail.com>
2021-03-27 23:23:21 +0100

    Added Paper to structure, need add media query

--- src/presentation/web/src/components/contactPage/ContactPage.tsx
+++ src/presentation/web/src/components/contactPage/ContactPage.tsx
@@ -6,26 +6,28 @@ import PersonOutlineOutlinedIcon from '@material-ui/icons/PersonOutlineOutlined'
 import { makeStyles } from '@material-ui/styles';
 import theme from "../../themes/theme";
 import Grid from '@material-ui/core/Grid';
-import mapPhoto from "./img/map.jpg";
 import shelterPhoto from "./img/hand.jpg";
-import SvgIcon from '@material-ui/icons/CallOutlined';
-import { Container, Typography } from '@material-ui/core';
-import { FullscreenExitTwoTone } from '@material-ui/icons';
+import { Paper, Typography } from '@material-ui/core';
 
 const useStyles = makeStyles({
     root: {
         flexGrow: 1,
-        marginTop: '8%',
         marginBottom: '5%',
+        marginTop: '1%',
     },
     iconContainer: {
         display: 'flex',
         flexDirection: 'column',
         borderRadius: 'rounded-full',
         alignItems: 'center',
+        justifyContent: 'center',
+        backgroundColor: theme.palette.background.paper,
+        paddingBottom: '12%',
     },
     lockBackground: {
         backgroundColor: theme.palette.secondary.dark,
+        marginTop: '5%',
+        marginBottom: '2%',
         display: 'flex',
         justifyContent: 'center',
         alignItems: 'center',
@@ -37,9 +39,17 @@ const useStyles = makeStyles({
         color: '#FFF',
         opacity: .87,
     },
+    shelterContainer: {
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'flex-end',
+    },
     shelterPhoto: {
         borderRadius: '15px',
-        width: '70%',
+        maxWidth: '70%',
+        alignItems: 'flex-end',
+        padding: 0,
+        ustify: 'center',
     },
     mapPhoto: {
         display: 'flex',
@@ -49,40 +59,51 @@ const useStyles = makeStyles({
         allowFullScreen: '',
         tabindex: '0',
         margin: 'auto',
-        padding: 0,
-        width: 600,
-        height: 450,
+        marginTop: '5%',
+        width: '100%',
+        height: '600px',
         loading: "lazy",
     },
 });
 
+
+
 const ContactPage: React.FC = () => {
     const classes = useStyles();
+    // const isTabletOrMobile = useMediaQuery({ query: '(max-width: 1220px)' })
     return (
-        <Grid container spacing={3} className={classes.root}>
-            <Grid item xs={12} sm={6} className={classes.iconContainer}>
-                <div className={classes.lockBackground}>
-                    <CallOutlinedIcon className={classes.lockIcon} />
-                </div>
-                <Typography>diusahidusa</Typography>
-                <div className={classes.lockBackground}>
-                    <LocationOnOutlinedIcon className={classes.lockIcon} />
-                </div>
-                <Typography>diusahidusa</Typography>
-                <div className={classes.lockBackground}>
-                    <AlternateEmailOutlinedIcon className={classes.lockIcon} />
-                </div>
-                <Typography>diusahidusa</Typography>
-                <div className={classes.lockBackground}>
-                    <PersonOutlineOutlinedIcon className={classes.lockIcon} />
-                </div>
-                <Typography>diusahidusa</Typography>
-            </Grid>
+        <Grid container spacing={0} className={classes.root}>
             <Grid item xs={12} sm={6}>
+                <Paper variant="outlined" className={classes.iconContainer}>
+                    <div className={classes.lockBackground}>
+                        <CallOutlinedIcon className={classes.lockIcon} />
+                    </div>
+                    <Typography variant={'subtitle2'}>Telefon:</Typography>
+                    <Typography variant={'subtitle2'}>111-222-333</Typography>
+                    <div className={classes.lockBackground}>
+                        <LocationOnOutlinedIcon className={classes.lockIcon} />
+                    </div>
+                    <Typography variant={'subtitle2'}>Adres:</Typography>
+                    <Typography variant={'subtitle2'}>ul.Główna 50c</Typography>
+                    <Typography variant={'subtitle2'}>51-180 Psary</Typography>
+                    <div className={classes.lockBackground}>
+                        <AlternateEmailOutlinedIcon className={classes.lockIcon} />
+                    </div>
+                    <Typography variant={'subtitle2'}>E-mail:</Typography>
+                    <Typography variant={'subtitle2'}>schronisko@schronisko-zlapki.com</Typography>
+                    <div className={classes.lockBackground}>
+                        <PersonOutlineOutlinedIcon className={classes.lockIcon} />
+                    </div>
+                    <Typography variant={'subtitle2'}>Dyrektor: Mateusz Kaczmarek</Typography>
+                    <Typography variant={'subtitle2'}>Główny weterynarz: Andrzej Śliwowski</Typography>
+                    <Typography variant={'subtitle2'}>Administracja: Grażyna Kowal</Typography>
+                </Paper>
+            </Grid>
+            <Grid item xs={12} sm={6} className={classes.shelterContainer} >
                 <img src={shelterPhoto} alt="shelter photo" className={classes.shelterPhoto} />
             </Grid>
             <Grid item xs={12}>
-                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d5147.8280012549485!2d17.02881503620837!3d51.18886271981003!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x470febdae76d444f%3A0x173c5b990e04fcc2!2zUHNhcnkgNTEtMTgwLCBHxYLDs3duYSA1MGM!5e0!3m2!1spl!2spl!4v1616777981166!5m2!1spl!2spl" className={classes.mapPhoto} aria-hidden="false"></iframe>
+                <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d20004.80284408307!2d17.0311458676758!3d51.189588443602666!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x173c5b990e04fcc2!2zUHNhcnkgNTEtMTgwLCBHxYLDs3duYSA1MGM!5e0!3m2!1spl!2spl!4v1616853103897!5m2!1spl!2spl" className={classes.mapPhoto} aria-hidden="false"></iframe>
             </Grid>
         </Grid>
     );
