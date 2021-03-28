5d37dba9659079d35e9a084d9e73fb2ec34aaa73
Tomasz Dudek <t.dudeek@gmail.com>
2021-03-28 20:23:18 +0200

    added media queries

--- src/presentation/web/src/components/contactPage/ContactPage.tsx
+++ src/presentation/web/src/components/contactPage/ContactPage.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import useMediaQuery from '@material-ui/core/useMediaQuery';
 import CallOutlinedIcon from '@material-ui/icons/CallOutlined';
 import LocationOnOutlinedIcon from '@material-ui/icons/LocationOnOutlined';
 import AlternateEmailOutlinedIcon from '@material-ui/icons/AlternateEmailOutlined';
@@ -7,13 +8,27 @@ import { makeStyles } from '@material-ui/styles';
 import theme from "../../themes/theme";
 import Grid from '@material-ui/core/Grid';
 import shelterPhoto from "./img/hand.jpg";
-import { Paper, Typography } from '@material-ui/core';
+import { createMuiTheme, Paper, Typography } from '@material-ui/core';
+import { Repeat } from '@material-ui/icons';
+
+const themes = createMuiTheme({
+    breakpoints: {
+        values: {
+            xs: 0,
+            sm: 600,
+            md: 800,
+            lg: 1000,
+            xl: 1280,
+        },
+    },
+});
 
 const useStyles = makeStyles({
     root: {
-        flexGrow: 1,
+        flexGrow: 0,
+        flexShrink: 0,
         marginBottom: '5%',
-        marginTop: '1%',
+        marginTop: '10%',
     },
     iconContainer: {
         display: 'flex',
@@ -23,10 +38,17 @@ const useStyles = makeStyles({
         justifyContent: 'center',
         backgroundColor: theme.palette.background.paper,
         paddingBottom: '12%',
+        [themes.breakpoints.down('md')]: {
+            paddingBottom: '5%',
+        },
+        [themes.breakpoints.down('sm')]: {
+            marginTop: '5%',
+            marginBottom: '5%',
+        },
     },
     lockBackground: {
         backgroundColor: theme.palette.secondary.dark,
-        marginTop: '5%',
+        marginTop: '2%',
         marginBottom: '2%',
         display: 'flex',
         justifyContent: 'center',
@@ -43,14 +65,35 @@ const useStyles = makeStyles({
         display: 'flex',
         alignItems: 'center',
         justifyContent: 'flex-end',
+        [themes.breakpoints.down('md')]: {
+            padding: 0,
+        },
+        [themes.breakpoints.down('xs')]: {
+            justifyContent: 'center',
+        },
     },
     shelterPhoto: {
         borderRadius: '15px',
-        maxWidth: '70%',
+        maxWidth: '500px',
+        maxHeight: '544px',
         alignItems: 'flex-end',
         padding: 0,
         ustify: 'center',
+        flexShrink: 0,
+        [themes.breakpoints.down('lg')]: {
+            height: '500px',
+        },
+        [themes.breakpoints.down('md')]: {
+            height: '430px',
+        },
+        [themes.breakpoints.down('sm')]: {
+            height: '370px',
+        },
+        [themes.breakpoints.down('xs')]: {
+            height: '400px',
+        },
     },
+
     mapPhoto: {
         display: 'flex',
         border: '0',
@@ -72,7 +115,7 @@ const ContactPage: React.FC = () => {
     const classes = useStyles();
     // const isTabletOrMobile = useMediaQuery({ query: '(max-width: 1220px)' })
     return (
-        <Grid container spacing={0} className={classes.root}>
+        <Grid container className={classes.root}>
             <Grid item xs={12} sm={6}>
                 <Paper variant="outlined" className={classes.iconContainer}>
                     <div className={classes.lockBackground}>
@@ -105,7 +148,7 @@ const ContactPage: React.FC = () => {
             <Grid item xs={12}>
                 <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d20004.80284408307!2d17.0311458676758!3d51.189588443602666!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x173c5b990e04fcc2!2zUHNhcnkgNTEtMTgwLCBHxYLDs3duYSA1MGM!5e0!3m2!1spl!2spl!4v1616853103897!5m2!1spl!2spl" className={classes.mapPhoto} aria-hidden="false"></iframe>
             </Grid>
-        </Grid>
+        </Grid >
     );
 };
 
