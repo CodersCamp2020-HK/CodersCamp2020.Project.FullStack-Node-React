966987cc4082fd99c51018097846999e8cf03455
Mateusz Król <matek8952@gmail.com>
2021-03-28 21:05:56 +0200

    Refactor

--- src/presentation/web/src/pages/Auth.tsx
+++ src/presentation/web/src/pages/Auth.tsx
@@ -1,10 +1,9 @@
 import React from 'react';
-import { Route, Switch, useRouteMatch } from 'react-router-dom';
+import { Route, Switch, useRouteMatch, Redirect } from 'react-router-dom';
 import ChangePassword from '../components/changePassword/ChangePassword';
 import ForgetPassword from '../components/auth/forgetPassword/ForgetPassword';
 import LoginForm from '../components/auth/loginForm/LoginForm';
 import RegisterPage from '../components/auth/registerPage/RegisterPage';
-import NotFound from './NotFound';
 
 const Auth: React.FC = () => {
     const { path } = useRouteMatch();
@@ -14,7 +13,7 @@ const Auth: React.FC = () => {
             <Route exact path={`${path}/forget`} component={ForgetPassword} />
             <Route exact path={`${path}/change`} component={ChangePassword} />
             <Route exact path={path} component={LoginForm} />
-            <Route path='*' component={NotFound} />
+            <Redirect to={`/404/${path}`} />
         </Switch>
     )
 }
