deebad274710ba104c205f309ed2481ebd3e778f
Haivex <demozial@gmail.com>
2021-03-29 16:49:33 +0200

    Added relations

--- src/application/OrganizationUsersService.ts
+++ src/application/OrganizationUsersService.ts
@@ -6,7 +6,18 @@ export class OrganizationUsersService {
 
     public async getAllUsers(role?: string): Promise<OrganizationUser[]> {
         console.log(role);
-        const organizationUsersQuery = this.organizationUsersRepository.createQueryBuilder();
+        const organizationUsersQuery = this.organizationUsersRepository
+            .createQueryBuilder('organization-user')
+            .leftJoin('organization-user.user', 'user')
+            .leftJoin('organization-user.organization', 'organization')
+            .select([
+                'organization-user',
+                'organization.id',
+                'organization.name',
+                'user.id',
+                'user.name',
+                'user.surname',
+            ]);
 
         if (role) {
             organizationUsersQuery.andWhere('role = :role', { role });
