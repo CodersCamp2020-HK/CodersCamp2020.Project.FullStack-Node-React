58dc238af3aa077b17eb3f548c3f8f4fc9d7b108
Haivex <demozial@gmail.com>
2021-03-29 19:10:49 +0200

    Added close behavior, button colors, logic

--- src/presentation/web/src/components/genericDialog/Dialog.tsx
+++ src/presentation/web/src/components/genericDialog/Dialog.tsx
@@ -1,30 +1,25 @@
-import React from 'react';
-import { Dialog as MuiDialog } from '@material-ui/core';
-import { DialogTitle } from '@material-ui/core';
-import { DialogContent } from '@material-ui/core';
-import { DialogActions } from '@material-ui/core';
+import { Button, Dialog as MuiDialog, DialogActions, DialogContent, DialogTitle } from '@material-ui/core';
+import React, { useState } from 'react';
 
 interface DialogProps {
     isOpen: boolean;
     title: string;
     content: string;
+    actionText: string;
 }
 
-const Dialog = ({isOpen, title, content}: DialogProps) => {
+const Dialog = ({ isOpen, title, content, actionText }: DialogProps) => {
+    const [open, setOpen] = useState<boolean>(isOpen);
     return (
-        <MuiDialog open={isOpen}>
-            <DialogTitle>
-                {title}
-            </DialogTitle>
-            <DialogContent>
-                {content}
-            </DialogContent>
+        <MuiDialog open={open} maxWidth="sm">
+            <DialogTitle>{title}</DialogTitle>
+            <DialogContent>{content}</DialogContent>
             <DialogActions>
-                <button>asd</button>
-                <button>zxc</button>
+                <Button variant='outlined' color='primary' onClick={() => setOpen(false)}>Powrót</Button>
+                <Button variant='contained' color='primary'>{actionText}</Button>
             </DialogActions>
         </MuiDialog>
-    )
-}
+    );
+};
 
 export default Dialog;
