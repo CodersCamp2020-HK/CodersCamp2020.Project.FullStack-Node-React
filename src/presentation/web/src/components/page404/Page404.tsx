f392a527e7ea2c77d73b0b4b581608e474ba6623
Ania-Em <aniaarchi@gmail.com>
2021-03-29 19:47:45 +0200

    Photos of good size

--- src/presentation/web/src/components/page404/Page404.tsx
+++ src/presentation/web/src/components/page404/Page404.tsx
@@ -11,64 +11,77 @@ const Page404 = () => {
    const theme = useTheme<Theme>();
     const useStyle = makeStyles({
         wrapper: {
-            marginTop: '5vh',
+            marginTop: '2vh',
             '& > *':{
-                marginTop: theme.spacing(4)
+                marginTop: theme.spacing(2)
             }
         },
-        paw404: {
-            width: '100%',
-            height: '100%',
-        },
         dogCatPhotos: {
-            width: '100%',
-            height: '100%',
+            display: 'flex',
+            justifyContent: 'space-between',
+            marginTop: theme.spacing(2),
         },
-        photo: {
+        photoCat: {
             width: '100%',
-            height: '100%',
+            height: '15vh',
             backgroundPosition: 'center',
+            backgroundSize: "contain",
+            backgroundImage: `url(${CatPhoto})`,
+            backgroundRepeat: 'no-repeat',
+        },
+        photoDog: {
+            width: '100%',
+            height: '15vh',
+            backgroundPosition: 'center',
+            backgroundSize: "contain",
+            backgroundImage: `url(${DogPhoto})`,
+            backgroundRepeat: 'no-repeat',
+        },
+        animation:{
+            height:'100%',
         }
     });
 
     const classes = useStyle();
 
 return (
-   <Grid spacing={4} container direction="column" item xs={10} sm={8} md={6} lg={4} justify="center" className={classes.wrapper}>
-        <Grid item className={classes.paw404}>
-            <PawIcon404 color={theme.palette.primary.main} width={140} height={140} colorText='white'/>
-         </Grid>
-         
-         <Typography variant="h6">Strona nie została odnaleziona</Typography>
-         <Typography variant="h5">Co tutaj robisz? Zgubiłeś/aś się?</Typography>
+<Grid container xs={12}>
+    <Grid item xs={1} className={classes.animation}>
 
+    </Grid>
+   <Grid container item direction="column" xs={10} sm={8} md={6} lg={4} justify="center" className={classes.wrapper}>
+        <Grid item >
+            <PawIcon404 color={theme.palette.primary.main} width={160} height={160} colorText='white'/>
+         </Grid>     
+         <Typography variant="h6" align="center">Strona nie została odnaleziona</Typography>
+         <Typography variant="h5" align="center">Co tutaj robisz? Zgubiłeś/aś się?</Typography>
          <Button
          variant="outlined" 
          color="primary" 
          size="large"
          fullWidth={true}
          endIcon={<UndoRounded>Go back</UndoRounded>}>
-        Wróc do poprzedniej strony
+            Wróc do poprzedniej strony
         </Button>
-
         <Button
          variant="contained" 
          color="primary" 
          size="large"
          fullWidth={true}
          endIcon={<Menu>Go to main page</Menu>}>
-        Przejdź do strony głównej
-        </Button>
-
-        <Grid item container className={classes.dogCatPhotos} >
-            <Grid item xs={2} >
-                <img src={CatPhoto} alt="cat photo" className={classes.photo}/>
+            Przejdź do strony głównej
+        </Button>  
+        <Grid container item xs={12} className={classes.dogCatPhotos} >
+            <Grid item xs={5} >
+                <div className={classes.photoCat}/>
             </Grid> 
-            <Grid item xs={2}>
-                <img src={DogPhoto} alt="dog photo" className={classes.photo}/>
+            <Grid item xs={3}>
+                <div className={classes.photoDog} />
             </Grid>     
-        </Grid>
+    </Grid> 
    </Grid>
+   
+</Grid>
 )
 }
 
