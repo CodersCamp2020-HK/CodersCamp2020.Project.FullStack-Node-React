a5f5e58fd1134fa385105fecfdb8a36dbb0edcfe
Haivex <demozial@gmail.com>
2021-03-29 16:21:03 +0200

    Added container

--- src/application/OrganizationUsersService.ts
+++ src/application/OrganizationUsersService.ts
@@ -1,7 +1,7 @@
 import OrganizationUser from '@infrastructure/postgres/OrganizationUser';
 import { Repository } from 'typeorm';
 
-export class AdoptionStepService {
+export class OrganizationUsersService {
     constructor(private organizationUsersRepository: Repository<OrganizationUser>) {}
 
     public async getAllUsers(role?: string): Promise<OrganizationUser[]> {}
