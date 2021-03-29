dc26274641e863b83dbfe514ffbe1951ffa2efa7
Haivex <demozial@gmail.com>
2021-03-29 18:51:22 +0200

    Added props and installed package in proper place

--- src/presentation/web/src/components/genericDialog/Dialog.tsx
+++ src/presentation/web/src/components/genericDialog/Dialog.tsx
@@ -3,14 +3,21 @@ import { Dialog as MuiDialog } from '@material-ui/core';
 import { DialogTitle } from '@material-ui/core';
 import { DialogContent } from '@material-ui/core';
 import { DialogActions } from '@material-ui/core';
-const Dialog = (open: boolean) => {
-    return (
-        <MuiDialog open={open}>
-            <DialogTitle>
 
+interface DialogProps {
+    isOpen: boolean;
+    title: string;
+    content: string;
+}
+
+const Dialog = ({isOpen, title, content}: DialogProps) => {
+    return (
+        <MuiDialog open={isOpen}>
+            <DialogTitle>
+                {title}
             </DialogTitle>
             <DialogContent>
-
+                {content}
             </DialogContent>
             <DialogActions>
                 <button>asd</button>
