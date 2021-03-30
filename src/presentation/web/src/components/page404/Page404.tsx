ad1cd73b2908e96bad41b63710d47121f433aa24
Ania-Em <aniaarchi@gmail.com>
2021-03-30 22:31:20 +0200

    Page404 refactor

--- src/presentation/web/src/components/page404/Page404.tsx
+++ src/presentation/web/src/components/page404/Page404.tsx
@@ -1,55 +1,54 @@
 import React from "react";
-import { Button, Grid, Theme, Typography, useTheme } from "@material-ui/core";
+import { Grid, Theme, Typography, useTheme } from "@material-ui/core";
 import { makeStyles } from "@material-ui/styles";
 import PawIcon404 from "./img/PawIcon404";
 import AnimationPaws from "./animationPaws/AnimationPaws";
 import CatPhoto from "./img/cat1.png";
 import DogPhoto from "./img/dog1.png";
 import Hidden from "@material-ui/core/Hidden";
-import BtnGoBack from './buttonGoBack/BtnGoBack'
-import BtnMainPage from './buttonMainPage/BtnMainPage'
+import BtnGoBack from "./buttonGoBack/BtnGoBack";
+import BtnMainPage from "./buttonMainPage/BtnMainPage";
 
-const Page404 = () => {
-  const theme = useTheme<Theme>();
-  const useStyle = makeStyles({
-    wrapper: {
-      display: "flex",
-    },
-    wrapper404: {
-      "& > *": {
-        marginTop: theme.spacing(2),
-      },
-    },
-    dogCatPhotos: {
-      display: "flex",
-      justifyContent: "space-between",
+const useStyle = makeStyles((theme: Theme) => ({
+  wrapper: {
+    display: "flex",
+  },
+  wrapper404: {
+    "& > *": {
       marginTop: theme.spacing(2),
     },
-    photoCat: {
-      width: "50%",
-      height: "20vh",
-      backgroundPosition: "left",
-      backgroundSize: "contain",
-      backgroundImage: `url(${CatPhoto})`,
-      backgroundRepeat: "no-repeat",
-      marginLeft: theme.spacing(2),
-    },
-    animation: {
-      height: "100%",
-    },
-    photoDog: {
-      width: "30%",
-      height: "20vh",
-      backgroundPosition: "right",
-      backgroundSize: "contain",
-      backgroundImage: `url(${DogPhoto})`,
-      backgroundRepeat: "no-repeat",
-      marginRight: theme.spacing(2),
-    },
-  });
+  },
+  dogCatPhotos: {
+    display: "flex",
+    justifyContent: "space-between",
+    marginTop: theme.spacing(2),
+  },
+  photoCat: {
+    width: "50%",
+    height: "20vh",
+    backgroundPosition: "left",
+    backgroundSize: "contain",
+    backgroundImage: `url(${CatPhoto})`,
+    backgroundRepeat: "no-repeat",
+    marginLeft: theme.spacing(2),
+  },
+  animation: {
+    height: "100%",
+  },
+  photoDog: {
+    width: "30%",
+    height: "20vh",
+    backgroundPosition: "right",
+    backgroundSize: "contain",
+    backgroundImage: `url(${DogPhoto})`,
+    backgroundRepeat: "no-repeat",
+    marginRight: theme.spacing(2),
+  },
+}));
 
+const Page404 = () => {
   const classes = useStyle();
-
+  const theme = useTheme();
   return (
     <Grid container xs={12} className={classes.wrapper}>
       <Grid item xs={1} className={classes.animation}>
@@ -88,8 +87,8 @@ const Page404 = () => {
           <Typography variant="h5" align="center">
             Co tutaj robisz? Zgubiłeś/aś się?
           </Typography>
-          <BtnGoBack/>
-          <BtnMainPage/>
+          <BtnGoBack />
+          <BtnMainPage />
         </Grid>
         <Grid container item xs={12} className={classes.dogCatPhotos}>
           <div className={classes.photoCat} />
