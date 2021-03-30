aead7c777ef40ebac995a27c1729a034bf802c3e
Ania-Em <aniaarchi@gmail.com>
2021-03-30 18:52:46 +0200

    Animation and buttons working

--- src/presentation/web/src/components/page404/Page404.tsx
+++ src/presentation/web/src/components/page404/Page404.tsx
@@ -1,20 +1,21 @@
 import React from "react";
 import { Button, Grid, Theme, Typography, useTheme } from "@material-ui/core";
 import { makeStyles } from "@material-ui/styles";
-import { UndoRounded, Menu } from "@material-ui/icons";
 import PawIcon404 from "./img/PawIcon404";
-import AnimationPaws from './animationPaws/AnimationPaws';
+import AnimationPaws from "./animationPaws/AnimationPaws";
 import CatPhoto from "./img/cat1.png";
 import DogPhoto from "./img/dog1.png";
+import Hidden from "@material-ui/core/Hidden";
+import BtnGoBack from './buttonGoBack/BtnGoBack'
+import BtnMainPage from './buttonMainPage/BtnMainPage'
 
 const Page404 = () => {
   const theme = useTheme<Theme>();
   const useStyle = makeStyles({
     wrapper: {
-      justifyContent: "space-between",
+      display: "flex",
     },
     wrapper404: {
-      marginTop: "2vh",
       "& > *": {
         marginTop: theme.spacing(2),
       },
@@ -25,87 +26,80 @@ const Page404 = () => {
       marginTop: theme.spacing(2),
     },
     photoCat: {
-      width: "100%",
-      height: "15vh",
-      backgroundPosition: "center",
+      width: "50%",
+      height: "20vh",
+      backgroundPosition: "left",
       backgroundSize: "contain",
       backgroundImage: `url(${CatPhoto})`,
       backgroundRepeat: "no-repeat",
+      marginLeft: theme.spacing(2),
     },
     animation: {
       height: "100%",
     },
     photoDog: {
-      width: "100%",
-      height: "15vh",
-      backgroundPosition: "center",
+      width: "30%",
+      height: "20vh",
+      backgroundPosition: "right",
       backgroundSize: "contain",
       backgroundImage: `url(${DogPhoto})`,
       backgroundRepeat: "no-repeat",
-    }
+      marginRight: theme.spacing(2),
+    },
   });
 
   const classes = useStyle();
-  
+
   return (
     <Grid container xs={12} className={classes.wrapper}>
       <Grid item xs={1} className={classes.animation}>
-        <AnimationPaws/>
+        <Hidden xsDown>
+          <AnimationPaws />
+        </Hidden>
       </Grid>
       <Grid
         container
         item
         direction="column"
         xs={10}
-        sm={8}
-        md={6}
-        lg={4}
         justify="center"
         className={classes.wrapper404}
       >
-        <Grid item>
+        <Grid
+          container
+          item
+          direction="column"
+          xs={12}
+          sm={10}
+          md={8}
+          lg={6}
+          justify="center"
+          className={classes.wrapper404}
+        >
           <PawIcon404
             color={theme.palette.primary.main}
             width={160}
             height={160}
             colorText="white"
           />
+          <Typography variant="h6" align="center">
+            Strona nie została odnaleziona
+          </Typography>
+          <Typography variant="h5" align="center">
+            Co tutaj robisz? Zgubiłeś/aś się?
+          </Typography>
+          <BtnGoBack/>
+          <BtnMainPage/>
         </Grid>
-        <Typography variant="h6" align="center">
-          Strona nie została odnaleziona
-        </Typography>
-        <Typography variant="h5" align="center">
-          Co tutaj robisz? Zgubiłeś/aś się?
-        </Typography>
-        <Button
-          variant="outlined"
-          color="primary"
-          size="large"
-          fullWidth={true}
-          endIcon={<UndoRounded>Go back</UndoRounded>}
-        >
-          Wróc do poprzedniej strony
-        </Button>
-        <Button
-          variant="contained"
-          color="primary"
-          size="large"
-          fullWidth={true}
-          endIcon={<Menu>Go to main page</Menu>}
-        >
-          Przejdź do strony głównej
-        </Button>
         <Grid container item xs={12} className={classes.dogCatPhotos}>
-          <Grid item xs={5}>
-            <div className={classes.photoCat} />
-          </Grid>
-          <Grid item xs={3}>
-            <div className={classes.photoDog} />
-          </Grid>
+          <div className={classes.photoCat} />
+          <div className={classes.photoDog} />
         </Grid>
       </Grid>
       <Grid item xs={1} className={classes.animation}>
-        <AnimationPaws/>
+        <Hidden xsDown>
+          <AnimationPaws />
+        </Hidden>
       </Grid>
     </Grid>
   );
--- src/presentation/web/src/components/page404/animationPaws/AnimationPaws.tsx
+++ src/presentation/web/src/components/page404/animationPaws/AnimationPaws.tsx
@@ -7,30 +7,20 @@ const useStyle = makeStyles({
   pawWrapper: {
     display: "flex",
     width: "100%",
-    height: "80vh",
+    height: "75vh",
     flexDirection: "column",
     justifyContent: "space-between",
-  },
-
- 
-
-  "@keyframes pawAnimation": {
-    "0%": {
-      opacity: 1,
-    },
-    "7%": {
-      opacity: 0,
-    },
-    "100%": {
-      opacity: 0,
-    },
+    margin: '2vh 0 3vh 0'
   },
 
   "@keyframes stepAnimation": {
     "0%": {
+      opacity: 0,
+    },
+    "10%": {
       opacity: 1,
     },
-    "7%": {
+    "30%": {
       opacity: 0,
     },
     "100%": {
@@ -41,34 +31,35 @@ const useStyle = makeStyles({
   pairPaw: {
     display: "flex",
     width: "100%",
-    justifyContent: "space-between", 
-    animation: `5000ms $stepsAnimation ease-in-out infinite`,
+    justifyContent: "space-between",
 
     "& > div:first-child > svg": {
-      animation: `5000ms $pawAnimation ease-in-out infinite`,
-      opacity: "0",
       transform: "rotate(-25deg) translateY(30px)",
     },
     "& > div:last-child > svg": {
-      animation: `5000ms $pawAnimation ease-in-out infinite`,
-      opacity: "0",
       transform: "rotate(15deg)",
-      animationDelay: '100ms',
     },
   },
+
+  pairPaw1: {
+    animation: '5500ms $stepAnimation 3s ease-in-out infinite',
+  },
+  pairPaw2: {
+    animation: '5500ms $stepAnimation 2.4s ease-in-out infinite',
+  },
+  pairPaw3: {
+    animation: '5500ms $stepAnimation 1.8s ease-in-out infinite',
+  },
+  pairPaw4: {
+    animation: '5500ms $stepAnimation 1.2s ease-in-out infinite',
+  },
+  pairPaw5: {
+    animation: '5500ms $stepAnimation 0.6s ease-in-out infinite',
+  },
+  pairPaw6: {
+    animation: '5500ms $stepAnimation 0s ease-in-out infinite',
+  },
 });
-
-const PairPaws = () => {
-  const theme = useTheme<Theme>();
-  const classes = useStyle();
-
-  return (
-    <div className={classes.pairPaw}>
-      <PawIcon color={theme.palette.primary.dark} width={15} height={15} />
-      <PawIcon color={theme.palette.primary.dark} width={15} height={15} />
-    </div>
-  );
-};
 
 const AnimationPaws = () => {
   const theme = useTheme<Theme>();
@@ -76,15 +67,30 @@ const AnimationPaws = () => {
 
   return (
     <div className={classes.pawWrapper}>
-      <PairPaws />
-      <PairPaws />
-      <PairPaws />
-      <PairPaws />
-      <PairPaws />
-      <PairPaws />
-      <PairPaws />
-      <PairPaws />
-      <PairPaws />
+      <div className={`${classes.pairPaw} ${classes.pairPaw1}`}>
+        <PawIcon color={theme.palette.primary.dark} width={15} height={15} />
+        <PawIcon color={theme.palette.primary.main} width={15} height={15} />
+      </div>
+      <div className={`${classes.pairPaw} ${classes.pairPaw2}`}>
+        <PawIcon color={theme.palette.primary.light} width={15} height={15} />
+        <PawIcon color={theme.palette.primary.dark} width={20} height={20} />
+      </div>
+      <div className={`${classes.pairPaw} ${classes.pairPaw3}`}>
+        <PawIcon color={theme.palette.primary.main} width={20} height={20} />
+        <PawIcon color={theme.palette.primary.dark} width={15} height={15} />
+      </div>
+      <div className={`${classes.pairPaw} ${classes.pairPaw4}`}>
+        <PawIcon color={theme.palette.primary.dark} width={25} height={25} />
+        <PawIcon color={theme.palette.primary.light} width={15} height={15} />
+      </div>
+      <div className={`${classes.pairPaw} ${classes.pairPaw5}`}>
+        <PawIcon color={theme.palette.primary.main} width={15} height={15} />
+        <PawIcon color={theme.palette.primary.dark} width={20} height={20} />
+      </div>
+      <div className={`${classes.pairPaw} ${classes.pairPaw6}`}>
+        <PawIcon color={theme.palette.primary.main} width={15} height={15} />
+        <PawIcon color={theme.palette.primary.dark} width={20} height={20} />
+      </div>
     </div>
   );
 };
