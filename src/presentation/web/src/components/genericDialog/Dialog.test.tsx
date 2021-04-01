1d1cb0fcc21c58520ba15e8fa874ab5cb33ed16b
Haivex <demozial@gmail.com>
2021-04-01 12:44:55 +0200

    Test hidden dialog

--- src/presentation/web/src/components/genericDialog/Dialog.test.tsx
+++ src/presentation/web/src/components/genericDialog/Dialog.test.tsx
@@ -4,53 +4,84 @@ import React from 'react';
 import muiWrapper from '../../testUtils/muiWrapper';
 import Dialog from './Dialog';
 
-const dialogProps = {
-    title: 'My dialog',
-    content: 'Lorem ipsum dor si amet',
-    actionText: 'Wyślij',
-    isOpen: true,
-};
-
-let documentBody: RenderResult;
-beforeEach(() => {
-    documentBody = render(
-        muiWrapper(
-            <Dialog
-                title={dialogProps.title}
-                content={dialogProps.content}
-                isOpen={dialogProps.isOpen}
-                actionText={dialogProps.actionText}
-                handleAction={mockHandleAction}
-            />,
-        ),
-    );
-});
-
 const mockHandleAction = jest.fn();
+let documentBody: RenderResult;
 
-it('show all text getted as props', () => {
-    expect(documentBody.getByText(dialogProps.title)).toBeInTheDocument();
-    expect(documentBody.getByText(dialogProps.content)).toBeInTheDocument();
-    expect(documentBody.getByText(dialogProps.actionText)).toBeInTheDocument();
-});
+describe('Hidden dialog', () => {
+    const dialogProps = {
+        title: 'My dialog',
+        content: 'Lorem ipsum dor si amet',
+        actionText: 'Wyślij',
+        isOpen: false,
+    };
 
-it('show return button', async () => {
-    const button = await documentBody.findByTestId('return');
-    expect(button).toBeInTheDocument();
-});
-
-it('hide dialog after click return button', async () => {
-    const button = await documentBody.findByTestId('return');
-    const dialogTitle = await documentBody.findByTestId('title');
-    fireEvent.click(button);
-    expect(dialogTitle).not.toBeVisible();
-});
-
-it('call action callback', async () => {
-    await act(async () => {
-        const button = await documentBody.findByTestId('action');
-        fireEvent.click(button);
+    beforeEach(() => {
+        documentBody = render(
+            muiWrapper(
+                <Dialog
+                    title={dialogProps.title}
+                    content={dialogProps.content}
+                    isOpen={dialogProps.isOpen}
+                    actionText={dialogProps.actionText}
+                    handleAction={mockHandleAction}
+                />,
+            ),
+        );
     });
 
-    expect(mockHandleAction).toBeCalledTimes(1);
+    it('hide all text getted as props', () => {
+        expect(documentBody.queryByText(dialogProps.title)).not.toBeInTheDocument();
+        expect(documentBody.queryByText(dialogProps.content)).not.toBeInTheDocument();
+        expect(documentBody.queryByText(dialogProps.actionText)).not.toBeInTheDocument();
+    });
+});
+
+describe('Dialog without textarea', () => {
+    const dialogProps = {
+        title: 'My dialog',
+        content: 'Lorem ipsum dor si amet',
+        actionText: 'Wyślij',
+        isOpen: true,
+    };
+
+    beforeEach(() => {
+        documentBody = render(
+            muiWrapper(
+                <Dialog
+                    title={dialogProps.title}
+                    content={dialogProps.content}
+                    isOpen={dialogProps.isOpen}
+                    actionText={dialogProps.actionText}
+                    handleAction={mockHandleAction}
+                />,
+            ),
+        );
+    });
+
+    it('show all text getted as props', () => {
+        expect(documentBody.getByText(dialogProps.title)).toBeInTheDocument();
+        expect(documentBody.getByText(dialogProps.content)).toBeInTheDocument();
+        expect(documentBody.getByText(dialogProps.actionText)).toBeInTheDocument();
+    });
+
+    it('show return button', async () => {
+        const button = await documentBody.findByTestId('return');
+        expect(button).toBeInTheDocument();
+    });
+
+    it('hide dialog after click return button', async () => {
+        const button = await documentBody.findByTestId('return');
+        const dialogTitle = await documentBody.findByTestId('title');
+        fireEvent.click(button);
+        expect(dialogTitle).not.toBeVisible();
+    });
+
+    it('call action callback', async () => {
+        await act(async () => {
+            const button = await documentBody.findByTestId('action');
+            fireEvent.click(button);
+        });
+
+        expect(mockHandleAction).toBeCalledTimes(1);
+    });
 });
