384463280fed9c07dd9cfb4bbf848f01ca852e2c
Mateusz Król <matek8952@gmail.com>
2021-04-02 00:57:17 +0200

    TextArea form

--- src/presentation/web/src/components/forms/Form.tsx
+++ src/presentation/web/src/components/forms/Form.tsx
@@ -2,6 +2,8 @@ import React, { useState } from 'react'
 import isArray from '../../utils/IsArray';
 import { FormQuestion } from '../../client/index';
 import RadioGroup from '../common/radioGroup/RadioGroup';
+import TextField from '@material-ui/core/TextField';
+import Typography from '@material-ui/core/Typography';
 
 interface Props {
     questions: FormQuestion[];
@@ -32,10 +34,20 @@ const generateInput = (questions: FormQuestion[]) => {
                 case 'radio':
                     for (const answer of question.placeholder.answer) answers.push({ content: answer })
                     console.log(answers)
-                    return (<RadioGroup key={question.id} name={question.id.toString()} values={answers} getCheckedOption={handleData} />)
+                    return (
+                        <div>
+                            <Typography>{question.question}</Typography>
+                            <RadioGroup key={question.id} name={question.id.toString()} values={answers} getCheckedOption={handleData} />
+                        </div>
+                    )
             }
         }
-        return (<div key='elo'>ELOOOO</div>);
+        return (
+            <div>
+                <Typography>{question.question}</Typography>
+                <TextField key={question.id} name={question.id.toString()} multiline rows={2} rowsMax={4} />
+            </div>
+        );
     })
 }
 
