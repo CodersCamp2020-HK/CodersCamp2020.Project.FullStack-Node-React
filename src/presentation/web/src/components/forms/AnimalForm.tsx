4556223dc676be7a47ab59ab4986cc9b722283d6
Mateusz Król <matek8952@gmail.com>
2021-04-02 00:42:32 +0200

    Radio button in form

--- src/presentation/web/src/components/forms/AnimalForm.tsx
+++ src/presentation/web/src/components/forms/AnimalForm.tsx
@@ -3,7 +3,7 @@ import { useGetForm } from '../../client/index';
 import Form from './Form';
 
 const AnimalForm = () => {
-    const form = useGetForm({ animalId: 1, requestOptions: { headers: { access_token: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYWRtaW4iLCJpZCI6MSwiaWF0IjoxNjE3MjkwNDkxfQ.tyfJuo4lnPrZ3Jub_oiKKPw5T0BXa1hBiy3ndpSIoJY' }} });
+    const form = useGetForm({ animalId: 1, requestOptions: { headers: { access_token: '' }} });
 
     if (form.data && form.data.form) {
         return <Form questions={form.data.form.questions}/>
