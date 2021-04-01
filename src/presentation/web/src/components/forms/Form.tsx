4bc89a699e83e9f71e3c761132c949dd6c24d7be
Mateusz Król <matek8952@gmail.com>
2021-04-01 18:00:25 +0200

    Form

--- src/presentation/web/src/components/forms/Form.tsx
+++ src/presentation/web/src/components/forms/Form.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { FormQuestion, AnswerType } from '../../client/index';
+import RadioGroup from '../common/radioGroup/RadioGroup';
 
 interface Props {
     questions: FormQuestion[];
@@ -9,7 +10,14 @@ const Form: React.FC<Props> = ({ questions }) => {
     return (
         <div>
             {questions.map((question) => {
-                console.log(question.placeholder)
+                console.log(question.placeholder.answer)
+                // switch (question.placeholder.type) {
+                //     case 'radio':
+                //         return <RadioGroup values={question.placeholder.answer} name={question.id.toString()} getCheckedOption={} />
+                
+                //     default:
+                //         break;
+                // }
             })}
         </div>
     )
