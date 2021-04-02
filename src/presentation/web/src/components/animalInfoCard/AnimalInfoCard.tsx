42ce8707ace60a8013b05b2e0483733ab0cf90c6
Haivex <demozial@gmail.com>
2021-04-02 14:43:02 +0200

    Added get methods

--- src/presentation/web/src/components/animalInfoCard/AnimalInfoCard.tsx
+++ src/presentation/web/src/components/animalInfoCard/AnimalInfoCard.tsx
@@ -1,48 +1,54 @@
 import { Paper, Table, TableBody, TableCell, TableRow, Typography } from '@material-ui/core';
 import React from 'react';
+import { useGetAnimal, useGetAllWillignessesToAdoptCount } from '../../client/index'
 interface AnimalInfoCardProps {
     animalId: number
 }
-const AnimalInfoCard = ({animalId: number}: AnimalInfoCardProps) => {
-    const animalInfoRows = [{
-        title: 'Nr ewidencyjny:',
-        content: '2'
-    },
-    {
-        title: 'W schronisku od:',
-        content: '25/09/2021'
-    },
-    {
-        title: 'Wielkość:',
-        content: 'mały'
-    },
-    {
-        title: 'Akceptuje dzieci:',
-        content: 'tak'
-    },
-    {
-        title: 'Akceptuje zwierzęta:',
-        content: 'nie'
-    },
-    {
-        title: 'Wiek:',
-        content: '3'
-    },
-    {
-        title: 'Aktywność',
-        content: 'bardzo duża'
-    },
-    {
-        title: 'Ilość aplikujących',
-        content: '10'
-    },
-]
+const AnimalInfoCard = ({animalId}: AnimalInfoCardProps) => {
+    const {data: animal} = useGetAnimal({animalId})
+    const {data: aplicantNumber} = useGetAllWillignessesToAdoptCount({petName: animal.name})
+    let animalInfoRows;
+    if(animal) {
+        animalInfoRows = [{
+            title: 'Nr ewidencyjny:',
+            content: animal.id
+        },
+        {
+            title: 'W schronisku od:',
+            content: '29/04/2021'
+        },
+        {
+            title: 'Wielkość:',
+            content: 'mały'
+        },
+        {
+            title: 'Akceptuje dzieci:',
+            content: animal.acceptKids ? 'tak' : 'nie'
+        },
+        {
+            title: 'Akceptuje zwierzęta:',
+            content: animal.acceptAnimals ? 'tak' : 'nie'
+        },
+        {
+            title: 'Wiek:',
+            content:  animal.age
+        },
+        {
+            title: 'Aktywność',
+            content: 'bardzo duża'
+        },
+        {
+            title: 'Ilość aplikujących',
+            content: aplicantNumber.count
+        },
+    ]
+    }
     return (
         <Paper>
             <Typography style={{textAlign: 'center'}} variant='h2'>Zenek</Typography>
             <Table>
                 <TableBody>
-                    {animalInfoRows.map((singleRow, index) =>
+                    {animalInfoRows && animalInfoRows.map((singleRow, index) =>
                         <TableRow key={index}>
                             <TableCell component="th">
                                 <Typography variant='body2'>{singleRow.title}</Typography> 
