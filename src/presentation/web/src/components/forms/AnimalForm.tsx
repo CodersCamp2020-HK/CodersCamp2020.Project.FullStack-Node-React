770328f66fbd5b8f1526f7f0569a5a6ebaae0143
Mateusz Król <matek8952@gmail.com>
2021-04-02 12:39:02 +0200

    Checkbox form

--- src/presentation/web/src/components/forms/AnimalForm.tsx
+++ src/presentation/web/src/components/forms/AnimalForm.tsx
@@ -1,12 +1,17 @@
 import React from 'react';
 import { useGetForm } from '../../client/index';
 import Form from './Form';
+import Grid from '@material-ui/core/Grid';
 
 const AnimalForm = () => {
-    const { data } = useGetForm({ animalId: 1, requestOptions: { headers: { access_token: '' }} });
+    const { data } = useGetForm({ animalId: 1, requestOptions: { headers: { access_token: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYWRtaW4iLCJpZCI6MSwiaWF0IjoxNjE3MzQ2NzYzfQ.h3t7y8edtFxLAm46FNOjpUaaVyvYhLCVBrqx68rOMfc' }} });
 
     if (data && data.form) {
-        return <Form questions={data.form.questions}/>
+        return (
+            <Grid item sm={12} lg={8}>
+                <Form key='form' questions={data.form.questions}/>
+            </Grid>
+        )
     }
     return <></>
 }
--- src/presentation/web/src/components/forms/Form.tsx
+++ src/presentation/web/src/components/forms/Form.tsx
@@ -4,6 +4,7 @@ import { FormQuestion } from '../../client/index';
 import RadioGroup from '../common/radioGroup/RadioGroup';
 import TextField from '@material-ui/core/TextField';
 import Typography from '@material-ui/core/Typography';
+import CheckboxGroup from '../common/checkboxGroup/CheckboxGroup';
 
 interface Props {
     questions: FormQuestion[];
@@ -22,33 +23,44 @@ interface FormData {
     [key: string]: Array<CheckboxOption | RadioOption>;
 }
 
-const handleData = (name: string, data: string | CheckboxOption[]) => {
+const handleRadioData = (name: string, data: string) => {
+    console.log(name, data)
+}
+
+const handleCheckboxData = (name: string, data: CheckboxOption[]): void => {
     console.log(name, data)
 }
 
 const generateInput = (questions: FormQuestion[]) => {
     return questions.map((question) => {
         if (isArray(question.placeholder.answer)) {
-            const answers: Array<CheckboxOption | RadioOption> = []
+            const radioAnswers: RadioOption[] = []
+            const checkboxAnswers: CheckboxOption[] = []
             switch (question.placeholder.type) {
                 case 'radio':
-                    for (const answer of question.placeholder.answer) answers.push({ content: answer })
-                    console.log(answers)
+                    for (const answer of question.placeholder.answer) radioAnswers.push({ content: answer })
                     return (
-                        <div>
+                        <div key={`question${question.id}`}>
                             <Typography>{question.question}</Typography>
-                            <RadioGroup key={question.id} name={question.id.toString()} values={answers} getCheckedOption={handleData} />
+                            <RadioGroup key={question.id} name={question.id.toString()} values={radioAnswers} getCheckedOption={handleRadioData} />
                         </div>
                     )
                 case 'checkbox':
                     console.log('Checkbox');
+                    for (const answer of question.placeholder.answer) checkboxAnswers.push({ content: answer, checked: false, disabled: false })
+                    return (
+                        <div key={`question${question.id}`}>
+                            <Typography>{question.question}</Typography>
+                            <CheckboxGroup key={question.id} name={question.id.toString()} values={checkboxAnswers} getCheckedData={handleCheckboxData} />
+                        </div>
+                    )
                     break;
                 default:
                     throw new Error('Wrong type of question!')
             }
         }
         return (
-            <div>
+            <div key={`question${question.id}`}>
                 <Typography>{question.question}</Typography>
                 <TextField key={question.id} name={question.id.toString()} multiline rows={2} rowsMax={4} />
             </div>
