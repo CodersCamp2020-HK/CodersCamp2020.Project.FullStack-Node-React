a26fbce3156f48fa8ad9b15034ced978b3d40ade
Haivex <demozial@gmail.com>
2021-04-03 14:47:14 +0200

    Added padding to description

--- src/presentation/web/src/components/animalInfoDescription/AnimalInfoDescription.tsx
+++ src/presentation/web/src/components/animalInfoDescription/AnimalInfoDescription.tsx
@@ -1,14 +1,22 @@
-import { Paper, Typography } from '@material-ui/core';
+import { makeStyles, Paper, Theme, Typography } from '@material-ui/core';
 import React from 'react';
 import { useGetAnimal } from '../../client/index';
 interface AnimalInfoDescriptionProps {
     animalId: number;
 }
+
+const useStyles = makeStyles((theme: Theme) => ({
+    paper: {
+        padding: theme.spacing(2),
+    },
+}));
+
 const AnimalInfoDescription = ({ animalId }: AnimalInfoDescriptionProps) => {
     const { data: animal } = useGetAnimal({ animalId });
+    const styles = useStyles();
     return (
-        <Paper>
-            <Typography>{animal && animal.description}</Typography>
+        <Paper className={styles.paper}>
+            <Typography variant="body2">{animal && animal.description}</Typography>
         </Paper>
     );
 };
