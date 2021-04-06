3eca31b7dec334a5921f6d9e615dbb6e96a6f18f
Weronika Dziedzic <wera02@gmail.com>
2021-04-06 19:53:40 +0200

    add typography h3 to subheader

--- src/presentation/web/src/components/sideNav/SideNavList.tsx
+++ src/presentation/web/src/components/sideNav/SideNavList.tsx
@@ -1,6 +1,5 @@
 import List from '@material-ui/core/List';
 import MuiListItem from '@material-ui/core/ListItem';
-// import ListItem from '@material-ui/core/ListItem';
 import ListItemIcon from '@material-ui/core/ListItemIcon';
 import ListItemText from '@material-ui/core/ListItemText';
 import ListSubheader from '@material-ui/core/ListSubheader';
@@ -70,7 +69,7 @@ const SideNavList: React.FC<Props> = ({ name, user }) => {
 
     return (
         <div className={classes.root}>
-            <List subheader={<ListSubheader color="primary" >Cześć {name}!</ListSubheader>} component="nav">
+            <List subheader={<ListSubheader color="primary"><h3>Cześć {name}!</h3></ListSubheader>} component="nav">
                 {(user === 'normalUser' ? textUser : user === 'workerUser' ? textWorker : textAdmin).map((t) => (
                     <ListItem
                         key={t.key}
