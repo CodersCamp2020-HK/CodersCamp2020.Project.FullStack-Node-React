ff0062f4aa066263158a452cead00dcb4eb8ad00
Mateusz Król <matek8952@gmail.com>
2021-04-07 12:08:53 +0200

    Animal id from query string

--- src/presentation/web/src/components/forms/AnimalForm/AnimalForm.tsx
+++ src/presentation/web/src/components/forms/AnimalForm/AnimalForm.tsx
@@ -1,12 +1,14 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useGetForm } from '../../../client/index';
+import { useRouteMatch, Redirect } from 'react-router-dom';
 import SurveyForm from '../SurveyForm/SurveyForm';
 import Grid from '@material-ui/core/Grid';
 import useQuery from '../../../utils/UseQuery';
 
 const AnimalForm = () => {
     const query = useQuery();
-    console.log(query);
+    const id = query.get('animalId');
+    console.log(id);
     const { data, loading } = useGetForm({ animalId: 1, requestOptions: { headers: { access_token: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYWRtaW4iLCJpZCI6MSwiaWF0IjoxNjE3MzQ2NzYzfQ.h3t7y8edtFxLAm46FNOjpUaaVyvYhLCVBrqx68rOMfc' }} });
     const handleSubmit = (data: any) => {
         console.log(data);
