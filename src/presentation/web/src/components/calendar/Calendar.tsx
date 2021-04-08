cdfc424007d1dc720b56f8863473bd71f2a001fa
Haivex <demozial@gmail.com>
2021-04-08 12:59:18 +0200

    Added selected date logic and style

--- src/presentation/web/src/components/calendar/Calendar.tsx
+++ src/presentation/web/src/components/calendar/Calendar.tsx
@@ -1,17 +1,35 @@
-import { Calendar as MuiCalendar, MuiPickersUtilsProvider } from '@material-ui/pickers';
 import DateFnsUtils from '@date-io/date-fns';
-import { addDays } from 'date-fns'
+import { Calendar as MuiCalendar, Day, MuiPickersUtilsProvider } from '@material-ui/pickers';
+import { MaterialUiPickersDate } from '@material-ui/pickers/typings/date';
+import { addDays, isToday, isSameDay } from 'date-fns';
 import plLocale from 'date-fns/locale/pl';
-import React from 'react';
+import React, { useState } from 'react';
 
 const Calendar = () => {
+    const [selectedDate, setSelectedDate] = useState<Date>(null!);
     return (
         <MuiPickersUtilsProvider utils={DateFnsUtils} locale={plLocale}>
-            <MuiCalendar disablePast minDate={new Date()} maxDate={addDays(new Date(), 14)} date={new Date()} onChange={(date) => {
-                console.log(date);
-            }} />
+            <MuiCalendar
+                disablePast
+                minDate={new Date()}
+                maxDate={addDays(new Date(), 14)}
+                date={new Date()}
+                onChange={(date: MaterialUiPickersDate) => {
+                    setSelectedDate(date as Date);
+                }}
+                renderDay={(day, selectedCalendarDate, dayInCurrentMonth) => {
+                    const isTodayDay = isToday(day as Date);
+                    const isSelected = isSameDay(day as Date, selectedDate);
+                    const markedDay = (
+                        <Day selected={isSelected} current={isTodayDay}>
+                            {day?.getDate()}
+                        </Day>
+                    );
+                    return markedDay;
+                }}
+            />
         </MuiPickersUtilsProvider>
-    )
+    );
 };
 
 export default Calendar;
