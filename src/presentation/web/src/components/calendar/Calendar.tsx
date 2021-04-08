5b0690a2e3d7339834d713caaff372786efb25b9
Haivex <demozial@gmail.com>
2021-04-08 13:25:53 +0200

    Added disabled days

--- src/presentation/web/src/components/calendar/Calendar.tsx
+++ src/presentation/web/src/components/calendar/Calendar.tsx
@@ -1,9 +1,9 @@
 import DateFnsUtils from '@date-io/date-fns';
 import { Calendar as MuiCalendar, Day, MuiPickersUtilsProvider } from '@material-ui/pickers';
 import { MaterialUiPickersDate } from '@material-ui/pickers/typings/date';
-import { addDays, isToday, isSameDay } from 'date-fns';
 import plLocale from 'date-fns/locale/pl';
 import React, { useState } from 'react';
+import { addDays, isSameDay, isToday, isWithinInterval, set, startOfDay, startOfToday } from 'date-fns';
 
 const Calendar = () => {
     const [selectedDate, setSelectedDate] = useState<Date>(null!);
@@ -13,15 +13,19 @@ const Calendar = () => {
                 disablePast
                 minDate={new Date()}
                 maxDate={addDays(new Date(), 14)}
-                date={new Date()}
+                date={startOfToday()}
                 onChange={(date: MaterialUiPickersDate) => {
                     setSelectedDate(date as Date);
                 }}
                 renderDay={(day, selectedCalendarDate, dayInCurrentMonth) => {
                     const isTodayDay = isToday(day as Date);
                     const isSelected = isSameDay(day as Date, selectedDate);
+                    const isDisabledDate = !isWithinInterval(startOfDay(day as Date), {
+                        start: startOfToday(),
+                        end: addDays(startOfToday(), 14),
+                    });
                     const markedDay = (
-                        <Day selected={isSelected} current={isTodayDay}>
+                        <Day disabled={isDisabledDate} selected={isSelected} current={isTodayDay}>
                             {day?.getDate()}
                         </Day>
                     );
