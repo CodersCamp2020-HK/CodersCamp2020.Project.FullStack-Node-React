999cdb28badf5767c17b899fd9c1fd164777a7a8
Haivex <demozial@gmail.com>
2021-04-08 11:52:00 +0200

    Added max date ( two weeks after today)

--- src/presentation/web/src/components/calendar/Calendar.tsx
+++ src/presentation/web/src/components/calendar/Calendar.tsx
@@ -1,12 +1,13 @@
 import { Calendar as MuiCalendar, MuiPickersUtilsProvider } from '@material-ui/pickers';
 import DateFnsUtils from '@date-io/date-fns';
+import { addDays } from 'date-fns'
 import plLocale from 'date-fns/locale/pl';
 import React from 'react';
 
 const Calendar = () => {
     return (
         <MuiPickersUtilsProvider utils={DateFnsUtils} locale={plLocale}>
-            <MuiCalendar disablePast minDate={new Date()} date={new Date()} onChange={(date) => {
+            <MuiCalendar disablePast minDate={new Date()} maxDate={addDays(new Date(), 14)} date={new Date()} onChange={(date) => {
                 console.log(date);
             }} />
         </MuiPickersUtilsProvider>
