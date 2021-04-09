ad6667495ed36282904fadc275c8f4aa64d048f8
Mateusz Król <matek8952@gmail.com>
2021-04-09 19:53:50 +0200

    Deleted acces token

--- src/presentation/web/src/components/forms/AnimalForm/AnimalForm.tsx
+++ src/presentation/web/src/components/forms/AnimalForm/AnimalForm.tsx
@@ -8,25 +8,23 @@ interface Props {
 }
 
 const AnimalForm: React.FC<Props> = ({ animalId }) => {
-    const { data, loading } = useGetForm({ animalId, requestOptions: { headers: { access_token: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYWRtaW4iLCJpZCI6MSwiaWF0IjoxNjE3MzQ2NzYzfQ.h3t7y8edtFxLAm46FNOjpUaaVyvYhLCVBrqx68rOMfc' }} });
+    const { data, loading, error } = useGetForm({ animalId, requestOptions: { headers: { access_token: '' }} });
     const handleSubmit = (data: any) => {
         console.log(data);
-    }
-    const defaultValues = {
-        question1: 'Tak',
-        question2: 'Tak',
-        question5: 'Elo',
-        question8: [
-            'Aktywny',
-            'Leniwy'
-        ]
     }
 
     if (!loading && data && data.form) {
         return (
             <Grid item sm={12} lg={8}>
-                <SurveyForm key='form' questions={data.form.questions} handleSubmit={handleSubmit} defaultValues={defaultValues} disabled={true} />
+                <SurveyForm key='form' questions={data.form.questions} handleSubmit={handleSubmit} />
             </Grid>
+        )
+    }
+    if (error) {
+        return (
+            <div>
+                <p>{error.message}</p>
+            </div>
         )
     }
     return (
