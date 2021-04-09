8c574ba3c76de495ad4ca8477e7f56fc0f7fe0ab
Haivex <demozial@gmail.com>
2021-04-09 14:03:05 +0200

    Added time buttons

---
+++ src/presentation/web/src/components/timePicker/TimePicker.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Chip, Theme} from '@material-ui/core';
+import { makeStyles } from '@material-ui/styles';
+
+interface TimePickerProps {
+    times: string[];
+}
+
+const TimePicker = ({times}: TimePickerProps) => {
+    return (
+        <div>
+            {times.map(time => <Chip label={time} variant='outlined' color='primary' />)}
+        </div>
+    )
+}
+
+
+
+export default TimePicker;
