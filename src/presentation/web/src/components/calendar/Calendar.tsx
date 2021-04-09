49a0a6fb007e7f881b7cbfab4004678ff550f7b1
Haivex <demozial@gmail.com>
2021-04-09 16:17:12 +0200

    Refactor -  function results in constants

--- src/presentation/web/src/components/calendar/Calendar.tsx
+++ src/presentation/web/src/components/calendar/Calendar.tsx
@@ -2,9 +2,14 @@ import DateFnsUtils from '@date-io/date-fns';
 import { makeStyles, Paper, Theme } from '@material-ui/core';
 import { Calendar as MuiCalendar, Day, MuiPickersUtilsProvider } from '@material-ui/pickers';
 import { MaterialUiPickersDate } from '@material-ui/pickers/typings/date';
-import { addDays, isSameDay, isToday, isWithinInterval, startOfDay, startOfToday } from 'date-fns';
+import { addDays, endOfDay, isSameDay, isToday, isWithinInterval, startOfDay, startOfToday } from 'date-fns';
 import plLocale from 'date-fns/locale/pl';
 import React, { useState } from 'react';
+
+interface CalendarProps {
+    getSelectedDate: (date: Date) => any;
+}
+
 
 const useStyles = makeStyles((theme: Theme) => ({
     todayDate: {
@@ -39,41 +44,43 @@ const useStyles = makeStyles((theme: Theme) => ({
     }
 }));
 
-interface CalendarProps {
-    getSelectedDate: (date: Date) => any;
-}
-
 const Calendar = ({ getSelectedDate }: CalendarProps) => {
     const styles = useStyles();
     const [selectedDate, setSelectedDate] = useState<Date>(null!);
+
+    const beginningOfToday = startOfToday();
+    const beginningOfTwoWeeksLaterDay = addDays(startOfToday(), 14);
+    const endOfTwoWeeksLaterDay = endOfDay(addDays(startOfToday(), 14));
+
     return (
         <Paper className={styles.paper}>
             <MuiPickersUtilsProvider utils={DateFnsUtils} locale={plLocale}>
                 <MuiCalendar
                 classes={{week: styles.week}}
                     disablePast
-                    minDate={new Date()}
-                    maxDate={addDays(new Date(), 14)}
-                    date={startOfToday()}
+                    minDate={beginningOfToday}
+                    maxDate={endOfTwoWeeksLaterDay}
+                    date={beginningOfToday}
                     onChange={(date: MaterialUiPickersDate) => {
                         setSelectedDate(date as Date);
                         getSelectedDate(date as Date);
                     }}
-                    renderDay={(day, selectedCalendarDate, dayInCurrentMonth) => {
+                    renderDay={(day) => {
+                        const beginningOfDay = startOfDay(day as Date);
                         const isTodayDay = isToday(day as Date);
                         const isSelected = isSameDay(day as Date, selectedDate);
-                        const isDisabledDate = !isWithinInterval(startOfDay(day as Date), {
-                            start: startOfToday(),
-                            end: addDays(startOfToday(), 14),
+                        const isDisabledDate = !isWithinInterval(beginningOfDay, {
+                            start: beginningOfToday,
+                            end: beginningOfTwoWeeksLaterDay
                         });
-                        const markedDay = (
+                        const dayComponent = (
                             <div className={isTodayDay ? styles.todayDate : undefined}>
                                 <Day disabled={isDisabledDate} selected={isSelected} current={isTodayDay}>
-                                    {day?.getDate()}
+                                    {day && day.getDate()}
                                 </Day>
                             </div>
                         );
-                        return markedDay;
+                        return dayComponent;
                     }}
                 />
             </MuiPickersUtilsProvider>
