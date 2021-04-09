64409fe7063da2ced47d6c711f29a3fc2b8250af
Haivex <demozial@gmail.com>
2021-04-09 14:49:41 +0200

    Added wrapper and margin

--- src/presentation/web/src/components/timePicker/TimePicker.tsx
+++ src/presentation/web/src/components/timePicker/TimePicker.tsx
@@ -1,19 +1,30 @@
-import React from 'react';
-import { Chip, Theme} from '@material-ui/core';
+import { Chip, Theme } from '@material-ui/core';
 import { makeStyles } from '@material-ui/styles';
+import React from 'react';
 
 interface TimePickerProps {
     times: string[];
 }
 
-const TimePicker = ({times}: TimePickerProps) => {
+const useStyles = makeStyles((theme: Theme) => ({
+    wrapper: {
+        display: 'flex',
+        justifyContent: 'center',
+    },
+    chip: {
+        marginRight: theme.spacing(1),
+    },
+}));
+
+const TimePicker = ({ times }: TimePickerProps) => {
+    const styles = useStyles();
     return (
-        <div>
-            {times.map(time => <Chip label={time} variant='outlined' color='secondary' />)}
+        <div className={styles.wrapper}>
+            {times.map((time) => (
+                <Chip className={styles.chip} label={time} variant="outlined" color="secondary" />
+            ))}
         </div>
-    )
-}
-
-
+    );
+};
 
 export default TimePicker;
