5ac0dad05949d009641fd91b08cb9955dd2eca0f
Haivex <demozial@gmail.com>
2021-04-12 16:10:43 +0200

    Files are saved in state

--- src/presentation/web/src/components/addPhotoInput/AddPhotoInput.tsx
+++ src/presentation/web/src/components/addPhotoInput/AddPhotoInput.tsx
@@ -1,19 +1,39 @@
-import React from 'react';
-import { Add } from '@material-ui/icons'
 import { Fab } from '@material-ui/core';
+import { Add } from '@material-ui/icons';
+import React, { useEffect, useState } from 'react';
+
+interface Photos {
+    fromDb: File[];
+    fromUser: File[];
+}
 
 const AddPhotoInput = () => {
+    const [photos, setPhotos] = useState<Photos>({ fromDb: [], fromUser: [] });
+
+    useEffect(() => {
+        console.log(photos);
+    }, [photos]);
+
+    const handleCapture = ({ currentTarget }: React.ChangeEvent<HTMLInputElement>) => {
+        const uploadedFiles = currentTarget.files as FileList;
+        const filesToSave: File[] = [];
+        for (let i = 0; i < uploadedFiles.length; i++) {
+            filesToSave.push(uploadedFiles.item(i) as File);
+        }
+        setPhotos((prev) => ({
+            ...prev,
+            fromUser: [...prev.fromUser, ...filesToSave],
+        }));
+    };
+
     return (
         <div>
-            <Fab color='secondary' component="label">
+            <Fab color="secondary" component="label">
                 <Add />
-                <input multiple
-                    hidden
-                    type="file"
-                    accept="image/*" />
+                <input multiple hidden type="file" accept="image/*" onChange={handleCapture} />
             </Fab>
         </div>
-    )
-}
+    );
+};
 
 export default AddPhotoInput;
