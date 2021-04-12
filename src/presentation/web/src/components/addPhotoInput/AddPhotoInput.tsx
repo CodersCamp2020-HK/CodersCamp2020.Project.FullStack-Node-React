a5096ac6278fd0ec3f414a489d33606e613db88d
Haivex <demozial@gmail.com>
2021-04-12 17:45:05 +0200

    Photos are convert into base64 and showed

--- src/presentation/web/src/components/addPhotoInput/AddPhotoInput.tsx
+++ src/presentation/web/src/components/addPhotoInput/AddPhotoInput.tsx
@@ -7,14 +7,39 @@ interface Photos {
     fromUser: File[];
 }
 
+interface PhotosBase {
+    basephotos: string[];
+}
+
 const AddPhotoInput = () => {
     const [photos, setPhotos] = useState<Photos>({ fromDb: [], fromUser: [] });
+    const [photosbase, setPhotosbase] = useState<PhotosBase>({ basephotos: [] });
+    const inputRef = React.useRef<HTMLInputElement>(null!);
 
     useEffect(() => {
-        console.log(photos);
+        const getData = async () => {
+            let photosBase64 = [];
+            photosBase64 = await Promise.all(
+                photos.fromUser.map(async (file) => {
+                    const fileBase = await file.arrayBuffer();
+                    const buffer = Buffer.from(fileBase);
+                    return buffer.toString('base64');
+                }),
+            );
+            return photosBase64;
+        };
+
+        const saveAsState = async () => {
+            const base64img = await getData();
+            setPhotosbase((prev) => ({
+                basephotos: [...base64img],
+            }));
+        };
+
+        saveAsState();
     }, [photos]);
 
-    const handleCapture = ({ currentTarget }: React.ChangeEvent<HTMLInputElement>) => {
+    const handleCapture = ({ currentTarget, target }: React.ChangeEvent<HTMLInputElement>) => {
         const uploadedFiles = currentTarget.files as FileList;
         const filesToSave: File[] = [];
         for (let i = 0; i < uploadedFiles.length; i++) {
@@ -24,14 +49,17 @@ const AddPhotoInput = () => {
             ...prev,
             fromUser: [...prev.fromUser, ...filesToSave],
         }));
+        inputRef.current.value = '';
     };
 
     return (
         <div>
             <Fab color="secondary" component="label">
                 <Add />
-                <input multiple hidden type="file" accept="image/*" onChange={handleCapture} />
+                <input multiple hidden type="file" accept="image/*" onChange={handleCapture} ref={inputRef} />
             </Fab>
+            {photosbase.basephotos.length > 0 &&
+                photosbase.basephotos.map((img, index) => <img key={index} alt="img" src={`data:image/png;base64, ${img}`} />)}
         </div>
     );
 };
