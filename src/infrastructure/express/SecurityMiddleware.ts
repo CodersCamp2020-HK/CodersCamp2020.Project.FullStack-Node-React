f76c87fdfd99d5760248a3e890a1eb9ebea1aaeb
Haivex <demozial@gmail.com>
2021-04-12 12:52:39 +0200

    Added contentSecurityPolicy

--- src/infrastructure/express/SecurityMiddleware.ts
+++ src/infrastructure/express/SecurityMiddleware.ts
@@ -1,12 +1,24 @@
-import helmet from 'helmet';
 import cors from 'cors';
+import helmet from 'helmet';
 
 interface SecurityOptions {
     isProductionEnv: boolean;
 }
 
 const useSecurity = ({ isProductionEnv }: SecurityOptions) => {
-    const middlewares = [helmet()];
+    const middlewares = [
+        helmet({
+            contentSecurityPolicy: {
+                directives: {
+                    ...helmet.contentSecurityPolicy.getDefaultDirectives(),
+                    'script-src': ["'self'", 'maps.googleapis.com'],
+                    'img-src': ["'self'", 'data:', 'maps.gstatic.com', '*.googleapis.com', '*.ggpht'],
+                    'frame-src': ["'self'", 'https://www.google.com'],
+                    'connect-src': ["'self'", 'https://coders-camp-schronisko.herokuap'],
+                },
+            },
+        }),
+    ];
     if (!isProductionEnv) {
         middlewares.push(cors({ origin: 'http://localhost:3000' }));
     }
