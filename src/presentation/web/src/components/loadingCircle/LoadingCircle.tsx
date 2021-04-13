105016c3c5735cdb2454abfdb71a0ff7fbbacd45
Haivex <demozial@gmail.com>
2021-04-13 16:03:36 +0200

    Added LoadingCircle component (center circle)

---
+++ src/presentation/web/src/components/loadingCircle/LoadingCircle.tsx
@@ -0,0 +1,23 @@
+import { CircularProgress, makeStyles } from '@material-ui/core';
+import React from 'react';
+
+const useStyles = makeStyles({
+    wrapper: {
+        marginTop: 40,
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+});
+
+const LoadingCircle = ({size}: {size: number | string}) => {
+    const styles = useStyles();
+    return (
+        <div className={styles.wrapper}>
+            <CircularProgress size={size} />
+        </div>
+    )
+}
+
+export default LoadingCircle;
