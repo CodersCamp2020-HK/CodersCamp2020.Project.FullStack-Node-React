2d815feb8c9e36b1232486374b91f2f8d87f5b23
Haivex <demozial@gmail.com>
2021-04-13 13:54:51 +0200

    Added file upload example

---
+++ src/presentation/web/src/components/addPhotoInput/fileUploadExample.tsx
@@ -0,0 +1,9 @@
+//Przyklad zeby sie nie meczyc z wysylaniem plikow do bazy
+// var formData = new FormData();
+// formData.append('photos', photos.fromUser[0]);
+// formData.append('photos', photos.fromUser[1]);
+// const { mutate: uploadPhotos } = useAddPhotos({
+//     animalId: 1,
+//     requestOptions: { body: formData, headers: [['access_token', localStorage.getItem('apiKey') as string]] },
+// });
+// uploadPhotos()
