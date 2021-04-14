6f0cf61f399369472b11749aef3ae6eea012c5ac
Haivex <demozial@gmail.com>
2021-04-14 18:03:18 +0200

    Added typography, margin, center

--- src/presentation/web/src/components/endAdoptionProcess/EndAdoptionProcess.tsx
+++ src/presentation/web/src/components/endAdoptionProcess/EndAdoptionProcess.tsx
@@ -1,6 +1,7 @@
-import React from 'react';
-import {Typography, Avatar, makeStyles, Theme} from '@material-ui/core';
+import { Avatar, makeStyles, Theme, Typography } from '@material-ui/core';
 import { Check } from '@material-ui/icons';
+import { format } from 'date-fns';
+import React from 'react';
 
 interface EndAdoptionProcessProps {
     submissionWorkerName: string;
@@ -12,24 +13,32 @@ const useStyles = makeStyles((theme: Theme) => ({
     wrapper: {
         display: 'flex',
         flexDirection: 'column',
+        alignItems: 'center',
     },
     text: {
-        marginTop: theme.spacing(8),
-    }
-}))
+        marginTop: theme.spacing(1),
+    },
+    avatar: {
+        backgroundColor: theme.palette.secondary.dark,
+    },
+}));
 
-const EndAdoptionProcess = ({submissionWorkerName, date, adoptionWorkerName}: EndAdoptionProcessProps) => {
+const EndAdoptionProcess = ({ submissionWorkerName, date, adoptionWorkerName }: EndAdoptionProcessProps) => {
     const styles = useStyles();
     return (
         <div className={styles.wrapper}>
-            <Check />
-            <Typography variant='h5'>Zakończenie procesu adopcyjnego?</Typography>
-            <Typography variant='h6'>Pracownik akceptujący wniosek adopcyjny:</Typography>
-            <Typography variant='h6'>{submissionWorkerName}</Typography>
-            <Typography variant='h6'>Data spotkania w schronisku</Typography>
-            <Typography variant='h6'>{date}</Typography>
-            <Typography variant='h6'>Pracownik akceptujący całą adopcję:</Typography>
-            <Typography variant='h6'>{adoptionWorkerName}</Typography>
+            <Avatar className={styles.avatar}>
+                <Check />
+            </Avatar>
+            <Typography variant="h5">Zakończenie procesu adopcyjnego?</Typography>
+            <Typography className={styles.text} variant="subtitle1">Pracownik akceptujący wniosek adopcyjny:</Typography>
+            <Typography variant="h6">{submissionWorkerName}</Typography>
+            <Typography  className={styles.text}  variant="subtitle1">Data spotkania w schronisku</Typography>
+            <Typography variant="h6">{format(date, 'dd/MM/yyyy, HH:mm')}</Typography>
+            <Typography  className={styles.text}  variant="subtitle1">Pracownik akceptujący całą adopcję:</Typography>
+            <Typography variant="h6">{adoptionWorkerName}</Typography>
         </div>
-    )
-}
+    );
+};
+
+export default EndAdoptionProcess;
