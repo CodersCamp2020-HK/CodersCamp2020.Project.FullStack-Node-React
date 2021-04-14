d2e5a2e09f702601a26edcd48d7b9de671d892b2
Haivex <demozial@gmail.com>
2021-04-14 19:52:19 +0200

    Size photo

--- src/presentation/web/src/pages/PageInProgress.tsx
+++ src/presentation/web/src/pages/PageInProgress.tsx
@@ -1,13 +1,24 @@
-import { Typography } from '@material-ui/core';
+import { makeStyles, Typography } from '@material-ui/core';
 import React from 'react';
 import UnderConstruction from '../assets/under-construction.jpg';
 import GridContainer from '../components/gridContainer/GridContainer';
 
+const useStyles = makeStyles({
+    photo: {
+        height: '70vh',
+    },
+    text: {
+        textAlign: 'center',
+    },
+});
 const PageInProgress = () => {
+    const styles = useStyles();
     return (
-        <GridContainer marginBottom={0} marginTop={0} spacing={2} align="center" justify="center">
-            <img alt="Work in progress" src={UnderConstruction} />
-            <Typography variant="h1">Strona w budowie! Zajrzyj później :)</Typography>
+        <GridContainer marginBottom={0} marginTop={0} spacing={0} align="center" justify="center">
+            <img className={styles.photo} alt="Work in progress" src={UnderConstruction} />
+            {/* <Typography className={styles.text} variant="h3">
+                Strona w budowie! Zajrzyj później :)
+            </Typography> */}
         </GridContainer>
     );
 };
