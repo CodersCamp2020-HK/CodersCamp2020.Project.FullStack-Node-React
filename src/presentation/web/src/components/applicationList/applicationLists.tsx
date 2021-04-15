55bd5527d22117be98e6e6b91b577b671afcdb95
Mateusz Król <37661247+KrolMateusz@users.noreply.github.com>
2021-04-15 16:03:21 +0200

    Worker list of application (#321)
    * created table with applications data
    
    * Application list cd
    
    * fix props and data type
    
    * add function to searched needy data (with type bug)
    
    * comment break function
    
    * Data are getted from db
    
    * Added animal id and submission id to cb
    
    * Removed application number
    
    * Added missing id
    
    Co-authored-by: Weronika <wera02@gmail.com>
    Co-authored-by: Haivex <demozial@gmail.com>

---
+++ src/presentation/web/src/components/applicationList/applicationLists.tsx
@@ -0,0 +1,76 @@
+import React from 'react';
+import { makeStyles, Theme } from '@material-ui/core/styles';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableContainer from '@material-ui/core/TableContainer';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
+import { ListData } from './listDisplay';
+import { Button } from '@material-ui/core';
+
+interface Props{
+    listData: ListData[],
+};
+
+const useStyles = makeStyles((theme: Theme) => ({
+    table: {
+      minWidth: 650,
+    },
+    button: {
+      background: theme.palette.secondary.main,
+      color: '#FFF',
+      borderRadius: 21,
+    }
+  }));
+
+function createData(dateOfSubmission: string, name: string, surname: string, animalId: number, submissionId: number) {
+    return { dateOfSubmission, name, surname, submissionId, animalId };
+  }
+  
+const Rows = ( props: Props ) => {
+      return props.listData.map((prop) => {
+        return (createData(prop.dateOfSubmission, prop.name, prop.surname, prop.animalId, prop.submissionId, ));
+        })
+    };
+
+const showApplication = (animalId: number, submissionId: number) => {
+  console.log('Animal id: ', animalId)
+  console.log('Submission id: ', submissionId)
+}    
+
+const ListOfApplications = (props:Props) => {
+    const classes = useStyles();
+    const rows = Rows(props);
+
+    return (
+    <Table className={classes.table} aria-label="simple table">
+            <TableHead>
+            <TableRow>
+                <TableCell>Data złożenia wniosku: </TableCell>
+                <TableCell align="right">Imię i nazwisko: </TableCell>
+                <TableCell align="right">Numer wniosku</TableCell>
+                <TableCell align="right"></TableCell>
+            </TableRow>
+            </TableHead>
+            <TableBody>
+            {rows.map((row) => (
+                <TableRow key={row.submissionId}>
+                <TableCell component="th" scope="row">
+                {row.dateOfSubmission}
+                </TableCell>
+                <TableCell align="right">{ row.name } { row.surname }</TableCell>
+                <TableCell align="right">{ row.submissionId }</TableCell>
+                <TableCell align="right">
+                  <Button onClick={() => showApplication(row.animalId, row.submissionId)} className={classes.button}>Zobacz wniosek</Button>
+                </TableCell>
+                </TableRow>
+            ))}
+            </TableBody>
+        </Table>
+    )
+
+  }
+
+  export default ListOfApplications;
---
+++ src/presentation/web/src/components/applicationList/listDisplay.tsx
@@ -0,0 +1,57 @@
+import { makeStyles, Paper, Theme } from '@material-ui/core';
+import React from 'react';
+import { FormAnimalSubmission, useGetAnimalSubmissionByAnimalId } from '../../client';
+import useQuery from '../../utils/UseQuery';
+import ListOfApplications from './applicationLists';
+
+const useStyle = makeStyles((theme: Theme) => ({
+    mainWrapper: {
+        width: '100%',
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
+        padding: '2rem 0'
+    },
+}))
+
+export interface ListData{
+    dateOfSubmission: string,
+    name: string,
+    surname: string,
+    applicationNumber: number,
+    animalId: number,
+    submissionId: number,
+};
+
+const ListDisplay = () => {
+    const classes = useStyle();
+    const id = useQuery().get('id');
+    const requestOptions = { headers: { access_token: localStorage.getItem('apiKey') ?? '' } };
+
+    const animalId = id ? parseInt(id) : 2;
+    const { data: listData } = useGetAnimalSubmissionByAnimalId ({ animalId ,requestOptions });
+
+    console.log(listData);
+    const shorterListData = ( data: FormAnimalSubmission[] ) => {
+        if (data)
+        return data.map((oneSubmission, index) => ({
+            dateOfSubmission: oneSubmission.submissionDate,
+            name: oneSubmission.applicant.name,
+            surname: oneSubmission.applicant.surname,
+            //applicationNumber: oneSubmission.answers[0].question.form.id,
+            animalId,
+            submissionId: oneSubmission.id,
+
+        }))
+    };
+    const dataToProps: ListData[] = shorterListData(listData as FormAnimalSubmission[]) as ListData[];
+
+    return(
+        <Paper className={classes.mainWrapper}>
+            {listData && <ListOfApplications listData={dataToProps}/>}
+        </Paper>
+    )  
+};
+
+export default ListDisplay;
