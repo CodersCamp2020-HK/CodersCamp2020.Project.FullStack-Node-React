3992dd9580a6bda9b6445d56a8677da426e346f6
Haivex <demozial@gmail.com>
2021-04-15 13:33:50 +0200

    Missing question mark

--- src/presentation/web/src/pages/AnimalInfo.tsx
+++ src/presentation/web/src/pages/AnimalInfo.tsx
@@ -25,7 +25,7 @@ const AnimalInfo = () => {
         photos.push(...base64Array);
     }
 
-    return !animal || !animal.readyForAdoption <NotFound /> : (
+    return !animal || !animal.readyForAdoption ? <NotFound /> : (
         <GridContainer marginBottom={0} marginTop={0} spacing={2} align="center" justify="center">
             <Grid item xs={12} sm={8}>
                 {loading ? <LoadingCircle size={70} /> : <Slider photos={[...photos]} />}
